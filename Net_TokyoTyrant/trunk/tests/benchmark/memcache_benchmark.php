97b4b5e7da7530b8d9372c5dafb03f60e46c628a
cocoiti <cocoiti@users.noreply.github.com>
2008-11-02 16:50:48 +0000

    ベンチマークを追加

---
+++ Net_TokyoTyrant/trunk/tests/benchmark/memcache_benchmark.php
@@ -0,0 +1,29 @@
+<?php
+require_once dirname(dirname(__FILE__)) . '/Net/TokyoTyrant.php';
+require_once 'Benchmark/Timer.php';
+$timer = new Benchmark_Timer();
+$timer->start();
+
+$memcache = new memcache();
+$timer->setMarker('create');
+$memcache->addServer('localhost');
+$timer->setMarker('connect');
+
+$data = 'aaaaaaa';
+for($i = 0;$i <= 10000 ;$i++){
+   $key = (string) rand(1,100);
+   $memcache->set($key, $data);
+}
+
+$timer->setMarker('put');
+
+for($i = 0;$i <= 10000 ;$i++){
+   $key = (string) rand(1,100);
+   $data = $memcache->get($key);
+}
+
+$timer->setMarker('get');
+
+$memcache->close();
+$timer->setMarker('close');
+$timer->display();
