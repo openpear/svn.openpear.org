97bdf0b056f30f5368cccf223ab9a5876b4311cd
brtriver <brtriver@224e63c0-5457-0410-989b-907d9cf58824>
2008-11-29 17:03:40 +0000

    commit /lib for release 0.1.0

---
+++ jpSimpleMailPlugin/trunk/lib/exception/jpSendMailException.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * jpSendMailException class
+ *
+ * @package    jpSimpleMailPlugin
+ * @subpackage exception
+ * @author     brt.river <brt.river@gmail.com>
+ * @version    $Id$
+ */
+class jpSendMailException extends sfException
+{
+}
