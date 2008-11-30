1bf685d91bafc6edbd439b3727aa075d17087ccf
anatoo <anatoo.jp@gmail.com>
2008-11-30 11:35:43 +0000

    サンプルコード追加。 PEGクラス追加。

--- PEG/trunk/code/PEG.php
+++ PEG/trunk/code/PEG.php
@@ -16,3 +16,124 @@ include_once dirname(__FILE__) . '/PEG/Not.php';
 include_once dirname(__FILE__) . '/PEG/Optional.php';
 include_once dirname(__FILE__) . '/PEG/Sequence.php';
 include_once dirname(__FILE__) . '/PEG/Token.php';
+
+/**
+ * PEG以下のクラスを生成するFactoryクラス
+ *
+ */
+class PEG
+{
+    /**
+     * @param string $str
+     * @return PEG_Context
+     */
+    static function context($str)
+    {
+        return new PEG_Context($str);
+    }
+    
+    /**
+     * @param callback $callback
+     * @param PEG_IParser $p
+     * @return PEG_CallbackAction
+     */
+    static function callbackAction($callback, PEG_IParser $p)
+    {
+        return new PEG_CallbackAction($callback, $p);
+    }
+    
+    /**
+     * @return PEG_Anything
+     */
+    static function anything()
+    {
+        return PEG_Anything::getInstance();
+    }
+    
+    /**
+     * @return PEG_Choice
+     */
+    static function choice()
+    {
+        return new PEG_Choice(func_get_args());
+    }
+    
+    /**
+     * @return PEG_EOS
+     */
+    static function eos()
+    {
+        return PEG_EOS::getInstance();
+    }
+    
+    /**
+     * @param PEG_IParser $p
+     * @return PEG_Not
+     */
+    static function not(PEG_IParser $p)
+    {
+        return new PEG_Not($p);
+    }
+    
+    /**
+     * @param PEG_IParser $p
+     * @return PEG_Optional
+     */
+    static function optional(PEG_IParser $p)
+    {
+        return new PEG_Optional($p);
+    }
+    
+    /**
+     * @return PEG_Sequence
+     */
+    static function sequence()
+    {
+        return new PEG_Sequence(func_get_args());
+    }
+    
+    /**
+     * @param PEG_IParser $p
+     * @return PEG_Many
+     */
+    static function many(PEG_IParser $p)
+    {
+        return new PEG_Many($p);
+    }
+    
+    /**
+     * @param PEG_IParser $p
+     * @return PEG_Many1
+     */
+    static function many1(PEG_IParser $p)
+    {
+        return new PEG_Many1($p);
+    }
+    
+    /**
+     * @param string $s
+     * @return PEG_Token
+     */
+    static function token($s)
+    {
+        return PEG_Token::get($s);
+    }
+    
+    /**
+     * @param PEG_IParser $p
+     * @return PEG_Lookahead
+     */
+    static function lookahead(PEG_IParser $p)
+    {
+        return new PEG_Lookahead($p);
+    }
+    
+    /**
+     * @param PEG_IParser $p
+     * @return PEG_LookaheadNot
+     */
+    static function lookaheadNot(PEG_IParser $p)
+    {
+        return new PEG_LookaheadNot($p);
+    }
+}
