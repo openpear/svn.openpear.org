03e4d9a09bd3fa58f93afdda39e40015080a0bdf
anatoo <anatoo.jp@gmail.com>
2008-12-02 17:34:49 +0000

    サンプル短くした

--- PEG/trunk/sample/sample01.php
+++ PEG/trunk/sample/sample01.php
@@ -2,30 +2,21 @@
 include_once dirname(__FILE__) . '/../code/PEG.php';
 
 /**
- * 括弧の対応をとる再帰的なパーサのサンプル
- * 認識した括弧を用いて文字列を階層化する
+ * 括弧の対応をとる再帰的なパーサのサンプル。
+ * 認識した括弧を用いて文字列を階層化する。
  * 
  * パーサのEBNFはこんな感じ
- * item := paren | anything
+ * item       := paren | anything
  * paren_item := (?! ")") item
- * paren := "(" paren_item* ")"
- * parser := item*
+ * paren      := "(" paren_item* ")"
+ * parser     := item*
  */
 
 
-$item = PEG::choice();
 $paren = PEG::ref();
-
-$item->with($paren)
-     ->with(PEG::anything());
-
-$paren_item_mat = PEG::sequence();
-$paren_item_mat->with(PEG::lookaheadNot(PEG::token(')')))
-               ->with($item);
-$paren_item = PEG::second($paren_item_mat);
-     
+$item = PEG::choice($paren, PEG::anything());
+$paren_item = PEG::andalso(PEG::not(PEG::token(')')), $item);
 $paren->set(PEG::pack(PEG::token('('), PEG::many($paren_item), PEG::token(')')));
-
 $parser = PEG::many($item);
 
 $str = 'abc(def(ghi)(jkl(mno)))';
