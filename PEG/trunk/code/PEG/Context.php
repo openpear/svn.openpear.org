ce4661a1d8e2f44e515c8afd1f541440479926cc
anatoo <anatoo.jp@gmail.com>
2008-12-02 16:10:06 +0000

    コメントちょいちょいつけてる

--- PEG/trunk/code/PEG/Context.php
+++ PEG/trunk/code/PEG/Context.php
@@ -1,9 +1,19 @@
 <?php
 
+/**
+ * PEG_IContextの実装クラス
+ *
+ * @see PEG::context()
+ */
 class PEG_Context implements PEG_IContext
 {
     protected $s, $i = 0, $len;
     
+    /**
+     * 与えられた文字列とその位置を保持するPEG_Contextインスタンスを生成する。
+     *
+     * @param string $s
+     */
     function __construct($s) { 
         $this->s = $s; 
         $this->len = strlen($s);
@@ -18,16 +28,6 @@ class PEG_Context implements PEG_IContext
         if ($this->eos() && $i > 0) throw new PEG_Failure();
         $this->i += $i;
         return substr($this->s, $this->i - $i, $i);
-    }
-    
-    /**
-     * @param int $i
-     * @return string
-     */
-    function lookahead($i)
-    {
-        if ($this->eos() && $i > 0) throw new PEG_Failure();
-        return substr($this->s, $this->i, $i);
     }
     
     /**
