ef2175702f9bca2fbfeb5a8c753bc75b8bc01cde
lagos <lagos@224e63c0-5457-0410-989b-907d9cf58824>
2008-12-06 11:39:39 +0000

    tapメソッド追加

--- PHP_Object/trunk/PHP/Object.php
+++ PHP_Object/trunk/PHP/Object.php
@@ -221,20 +221,10 @@ class PHP_Object
     protected function __call($name, $args = array())
     {   
         $args = array_map(array($this, 'revert'), $args);
-
-        if (!function_exists($name)) {
-            $aliases = str_replace('*', $name, $this->aliasMethods);
-            foreach ($aliases as $alias) {
-                if (function_exists($alias)) {
-                    $name = $alias;
-                    break;
-                }
-            }
-        }
+        $name = $this->aliasMethod($name);
 
         if (function_exists($name)) {
-            $offset = array_key_exists($name, $this->argOffsets) ? $this->argOffsets[$name] : 0;
-            if ($offset !== NULL) {
+            if (($offset = $this->argOffset($name)) !== NULL) {
                 array_splice($args, $offset, 0, array(&$this->data));
             }
             $result = call_user_func_array($name, $args);
@@ -245,49 +235,11 @@ class PHP_Object
                 return self::factory($result);
             }
         }
-        
-        switch ($name) {
-            case 'echo':
-                array_unshift($args, $this->data);
-                foreach ($args as $arg) {
-                    echo $arg;
-                }
-                return $this;
-            case 'print':
-                print $this->data;
-                return $this;
-            case 'isset':
-                array_unshift($args, $this->data);
-                foreach ($args as $arg) {
-                    if (!isset($arg)) {
-                        return self::factory(false);
-                    }
-                }
-                return self::factory(true);
-            case 'unset':
-                unset($this);
-                return self::factory();
-            case 'empty':
-                return self::factory(empty($this->data));
-            case 'eval':
-                return self::factory(eval($this->data));
-            case 'exit':
-                exit($this->data);
-            case 'die':
-                die($this->data);
-            case 'require':
-                require $this->data;
-                return $this;
-            case 'include':
-                include $this->data;
-                return $this;
-            case 'require_once':
-                require_once $this->data;
-                return $this;
-            case 'include_once':
-                include_once $this->data;
-                return $this;
+
+        if (($result = $this->callConstructAsMethod($name, $args)) != false) {
+            return $result;
         }
+        
     }
 
     protected function __get($name)
@@ -331,6 +283,66 @@ class PHP_Object
         }
     }
 
+    protected function aliasMethod($name) {
+        if (!function_exists($name)) {
+            $aliases = str_replace('*', $name, $this->aliasMethods);
+            foreach ($aliases as $alias) {
+                if (function_exists($alias)) {
+                    return $alias;
+                }
+            }
+        }
+        return $name;
+    }
+
+    protected function callConstructAsMethod($name, $args)
+    {
+        switch ($name) {
+            case 'echo':
+                array_unshift($args, $this->data);
+                foreach ($args as $arg) {
+                    echo $arg;
+                }
+                return $this;
+            case 'print':
+                print $this->data;
+                return $this;
+            case 'isset':
+                array_unshift($args, $this->data);
+                foreach ($args as $arg) {
+                    if (!isset($arg)) {
+                        return self::factory(false);
+                    }
+                }
+                return self::factory(true);
+            case 'unset':
+                unset($this);
+                return self::factory();
+            case 'empty':
+                return self::factory(empty($this->data));
+            case 'eval':
+                return self::factory(eval($this->data));
+            case 'exit':
+                exit($this->data);
+            case 'die':
+                die($this->data);
+            case 'require':
+                require $this->data;
+                return $this;
+            case 'include':
+                include $this->data;
+                return $this;
+            case 'require_once':
+                require_once $this->data;
+                return $this;
+            case 'include_once':
+                include_once $this->data;
+                return $this;
+            default:
+                return false;
+        }
+    }
+
     protected function revert($data) 
     {
         if ($data instanceof self) {
@@ -339,10 +351,34 @@ class PHP_Object
         return $data;
     }
 
+    public function argOffset($name) 
+    {
+        return array_key_exists($name, $this->argOffsets) ? $this->argOffsets[$name] : 0;
+    }
+
     public function val() 
     {
         return $this->data;
     }
+    
+    public function tap()
+    {
+        $args = func_get_args();
+        $args = array_map(array($this, 'revert'), $args);
+        $name = $this->aliasMethod(array_shift($args));
 
+        if (function_exists($name)) {
+            if (($offset = $this->argOffset($name)) !== NULL) {
+                array_splice($args, $offset, 0, array(&$this->data));
+            }
+            call_user_func_array($name, $args);
+            return $this;
+        }
+
+        if (($result = $this->callConstructAsMethod($name, $args)) != false) {
+            return $this;
+        }
+
+    }
 
 }
