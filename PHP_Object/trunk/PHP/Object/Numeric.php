8a9f4c7de2f0e553fbc40bddafb1becbc3b64a94
Naoyuki Kanezawa <naoyuki.kanezawa@gmail.com>
2008-12-14 17:49:50 +0000

    引数のオフセット設定追加

--- PHP_Object/trunk/PHP/Object/Numeric.php
+++ PHP_Object/trunk/PHP/Object/Numeric.php
@@ -4,7 +4,11 @@ require_once 'PHP/Object.php';
 class PHP_Object_Numeric extends PHP_Object
 {
 
-    protected $aliasMethods = array();
+    protected $aliasMethods = array(
+        'money_format' => 1,
+        'number_format' => 0,
+        'similar_text' => 1,
+    );
 
     public $argOffsets = array();
 
