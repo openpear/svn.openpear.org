9f847204cd84b7fa0cc44f794d168bf27125266f
lagos <lagos@224e63c0-5457-0410-989b-907d9cf58824>
2008-12-14 17:49:50 +0000

    引数のオフセット設定追加

--- PHP_Object/trunk/PHP/Object/Object.php
+++ PHP_Object/trunk/PHP/Object/Object.php
@@ -6,8 +6,18 @@ class PHP_Object_Object extends PHP_Object
     public $argOffsets =  array(
         'call_user_method_array' => 1,        
         'call_user_method' => 1,
+        'count' => 0,
+        'date_add' => 0,
+        'date_create' => 1,
+        'date_date_set' => 0,
+        'date_format' => 0,
+        'date_isodate_set' => 0,
+        'date_modify' => 0,
+        'date_offset_get' => 0,
+        'date_sub' => 0,
         'get_class' => 0,
         'get_parent_class' => 0,
+        'timezone_transitions_get' => 0,
     );
 
     public function __clone()
