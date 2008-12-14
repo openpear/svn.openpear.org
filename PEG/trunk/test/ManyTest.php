8f251816fd4bbca74555caf75531169fb3293f7c
anatoo <anatoo.jp@gmail.com>
2008-12-14 09:22:10 +0000

    テスト修正

--- PEG/trunk/test/ManyTest.php
+++ PEG/trunk/test/ManyTest.php
@@ -7,3 +7,7 @@ $many = PEG::many(PEG::token('a'));
 $context = PEG::context('aaaaaaaa');
 $lime->is($many->parse($context), array('a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'));
 $lime->is($context->tell(), 8);
+
+$context = PEG::context('ab');
+$parser = PEG::many(PEG::drop(PEG::token('ab')));
+$lime->is($parser->parse($context), array());
