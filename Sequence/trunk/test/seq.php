c4753150a4c8e4a06e2a0437f71a737e8ef9327f
anatoo <anatoo.jp@gmail.com>
2008-12-20 10:32:31 +0000

    テスト付け足し

--- Sequence/trunk/test/seq.php
+++ Sequence/trunk/test/seq.php
@@ -3,6 +3,7 @@ include_once dirname(__FILE__) . '/tool/lime.php';
 include_once dirname(__FILE__) . '/../src/seq.php';
 $lime = new lime_test;
 
+////////////////////////////////////////////////////////////////////////////////
 
 $lime->comment('count');
 $seq = seq();
@@ -10,6 +11,7 @@ $lime->is($seq->length, 0);
 $lime->is($seq->count(), 0);
 $lime->is(count($seq), 0);
 
+////////////////////////////////////////////////////////////////////////////////
 
 $lime->comment('ArrayAccess');
 $seq = seq(1, 2, 3, 4, 5);
@@ -25,3 +27,38 @@ unset($seq[-1]);
 $lime->ok(isset($seq[-1]));
 $lime->is($seq->toArray(), array(1, 2, 3, 4, 10));
 $lime->is(count($seq), 5);
+unset($seq[1]);
+$lime->is($seq->toArray(), array(1, 3, 4, 10));
+$lime->is(count($seq), 4);
+
+////////////////////////////////////////////////////////////////////////////////
+
+$lime->comment('pop');
+$seq = seq(1, 2, 3, 4);
+$lime->is($seq->pop(), 4);
+$seq->pop();
+$seq->pop();
+$seq->pop();
+$lime->is($seq->toArray(), array());
+try {
+    $seq->pop();
+    $lime->fail();
+}
+catch (RunTimeException $e) {
+    $lime->pass();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+$lime->comment('reverse');
+$seq = seq(1, 2, 3, 4);
+$lime->is($seq->reverse()->toArray(), array(4, 3, 2, 1));
+
+////////////////////////////////////////////////////////////////////////////////
+
+$lime->comment('lengthen');
+$seq = seq(1, 2, 3);
+$seq->lengthen(2);
+$lime->is(count($seq), 2);
+
+////////////////////////////////////////////////////////////////////////////////
