75c20a5185fdec3a674f73021af81d378b8ff149
halt <halt@224e63c0-5457-0410-989b-907d9cf58824>
2008-12-22 11:39:59 +0000

    ALL YOUR BASE ARE BELONG TO US!

--- Services_2chClient/trunk/source/2chClient.php
+++ Services_2chClient/trunk/source/2chClient.php
@@ -1,10 +1,46 @@
 <?php
+/**
+ * 2chClient.php
+ *
+ */
+
 require_once 'HTTP/Request.php';
 
-require_once 'Services/2chClient/Common.php';
-require_once 'Services/2chClient/BoardList.php';
-require_once 'Services/2chClient/Board.php';
+require_once dirname(__FILE__) . '/2chClient/BoardList.php';
+require_once dirname(__FILE__) . '/2chClient/Board.php';
 
+/**
+ * Services_2chClient
+ *
+ */
 class Services_2chClient
 {
+    /**
+     *
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * factory
+     *
+     */
+    public function factory($type)
+    {
+        $class_name = 'Services_2chClient_' . $type;
+
+        if (!class_exists($class_name)) {
+            throw new Exception("class: {$class_name} not found.");
+        }
+
+        $instance = new $class_name();
+
+        return $instance;
+    }
 }
+
+
+//$two_ch = new Services_2chClient();
+//$board = $two_ch->factory('Board');
+//$board->unntarakanntara();
