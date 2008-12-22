241a24911e9163d5754a7b706daa64b0b0349c6f
halt <halt@224e63c0-5457-0410-989b-907d9cf58824>
2008-12-22 12:38:19 +0000

    スレの内容とれたー

--- Services_2chClient/trunk/source/2chClient.php
+++ Services_2chClient/trunk/source/2chClient.php
@@ -8,6 +8,7 @@ require_once 'HTTP/Request.php';
 
 require_once dirname(__FILE__) . '/2chClient/BoardList.php';
 require_once dirname(__FILE__) . '/2chClient/Board.php';
+require_once dirname(__FILE__) . '/2chClient/Thread.php';
 
 /**
  * Services_2chClient
@@ -26,23 +27,34 @@ class Services_2chClient
      * factory
      *
      */
-    public function factory($type)
+    public function factory()
     {
-        $class_name = 'Services_2chClient_' . $type;
+        $args = func_get_args();
+
+        $class_name = 'Services_2chClient_' . $args[0];
 
         if (!class_exists($class_name)) {
             throw new Exception("class: {$class_name} not found.");
         }
 
-        $instance = new $class_name();
+        $instance = new $class_name($args[1], $args[2], $args[3]);
 
         return $instance;
     }
 }
 
 /*
+$host = 'gimpo.2ch.net';
+$board_key = 'namazuplus';
+$thread_key = 1226725210;
+        
 $two_ch = new Services_2chClient();
-$board = $two_ch->factory('Board');
-$result = $board->fetchThreadList();
-var_dump($result);
+
+//$board = $two_ch->factory('Board', $host, $board_key);
+//$result = $board->fetchThreadList();
+//var_dump($result);
+
+$thread = $two_ch->factory('Thread', $host, $board_key, $thread_key);
+//$data = $thread->fetch();
+//var_dump($data);
  */
--- Services_2chClient/trunk/source/2chClient/Thread.php
+++ Services_2chClient/trunk/source/2chClient/Thread.php
@@ -15,9 +15,44 @@ class Services_2chClient_Thread extends Services_2chClient_Common
     /**
      * __construct
      *
+     * @todo now implement
      */
     public function __construct($url, $board_key, $thread_key)
     {
+        parent::__construct($url, $board_key, $thread_key);
+
+        $url = 'http://' . $url . '/' . $board_key . '/dat/' . $thread_key . '.dat';
+        $body = $this->fetch($url);
+
+        
+        var_dump($body);
+    }
+
+    /**
+     * fetch
+     *
+     * @todo Commonへ
+     */
+    public function fetch($url)
+    {
+        $this->http_req->setURL($url);
+
+        if ($this->_lastModified) {
+            $httpObject->addHeader('If-Modified-Since', $this->_lastModified);
+        }
+
+        $response = $this->http_req->sendRequest();
+
+        if (PEAR::isError($response)) {
+            throw new Exception($response->getMessage());
+        }
+
+        $responseCode = $this->http_req->getResponseCode();
+        if ($responseCode != "200") {
+            throw new Exception("Invalid response code:{$responseCode}, url:{$url}");
+        }
+
+        return $this->http_req->getResponseBody();
     }
 }
 
