688691adccfb3c139ae1a01194aeaa66dbe6f6b5
halt <halt.feits@gmail.com>
2008-12-22 12:12:27 +0000

    スレが一番とりたいんです

---
+++ Services_2chClient/trunk/source/2chClient/Thread.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ *
+ */
+
+require_once dirname(__FILE__) . '/Common.php';
+
+/**
+ *
+ *
+ */
+class Services_2chClient_Thread extends Services_2chClient_Common
+{
+    /**
+     * __construct
+     *
+     */
+    public function __construct($url, $board_key, $thread_key)
+    {
+    }
+}
+
