e411f2e670dcae716d42fd10ace5b74cb40c6268
shuheitanuma <shuheitanuma@224e63c0-5457-0410-989b-907d9cf58824>
2008-12-25 20:27:50 +0000

    fix willcom address

--- Net_MobileCIDR/trunk/Net/MobileCIDR/Willcom.php
+++ Net_MobileCIDR/trunk/Net/MobileCIDR/Willcom.php
@@ -1,5 +1,5 @@
 <?php
 class Net_MobileCIDR_Willcom extends Net_MobileCIDR_Carrier{
-  protected $target_url = "http://www.willcom-inc.com/ja/service/contents_service/club_air_edge/for_phone/ip/";
+  protected $target_url = "http://www.willcom-inc.com/ja/service/contents_service/create/center_info/index.html";
   protected $regexp = '!<font size="2">([\d\./]+)</font>!';
 }
