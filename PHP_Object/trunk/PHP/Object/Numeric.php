13494120ebb8a20cded2b2e41abc49214f97bfd4
Naoyuki Kanezawa <naoyuki.kanezawa@gmail.com>
2008-12-29 13:55:11 +0000

    引数のオフセット設定修正、構造改善

--- PHP_Object/trunk/PHP/Object/Numeric.php
+++ PHP_Object/trunk/PHP/Object/Numeric.php
@@ -4,12 +4,4 @@ require_once 'PHP/Object.php';
 class PHP_Object_Numeric extends PHP_Object
 {
 
-    protected $aliasMethods = array(
-        'money_format' => 1,
-        'number_format' => 0,
-        'similar_text' => 1,
-    );
-
-    public $argOffsets = array();
-
 }
