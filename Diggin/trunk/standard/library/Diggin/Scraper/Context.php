37797cbd81c3e452ca3f0c7bbb03ffcaf5612211
sasezaki <sasezaki@224e63c0-5457-0410-989b-907d9cf58824>
2009-02-23 13:21:19 +0000

    依存モジュール指定ができる日がいつかきっと必ずくる！と念じながらコミット

---
+++ Diggin/trunk/standard/library/Diggin/Scraper/Context.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Diggin - Simplicity PHP Library
+ * 
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license.
+ * It is also available through the world-wide-web at this URL:
+ * http://diggin.musicrider.com/LICENSE
+ * 
+ * @category   Diggin
+ * @package    Diggin_Scraper
+ * @copyright  2006-2008 sasezaki (http://diggin.musicrider.com)
+ * @license    http://diggin.musicrider.com/LICENSE     New BSD License
+ */
+class Diggin_Scraper_Context
+{
+    private $_strategy;
+    
+    /**
+     * construct
+     * 
+     * @param Diggin_Scraper_Strategy_Abstract $strategy
+     */
+    public function __construct(Diggin_Scraper_Strategy_Abstract $strategy)
+    {
+        $this->_strategy = $strategy;
+    }
+
+    public function read()
+    {
+        return $this->_strategy->readResource();
+    }
+}
