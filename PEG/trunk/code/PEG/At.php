fbb61c0c1b5527095852a43c1a77bb19cbe952ec
anatoo <anatoo@224e63c0-5457-0410-989b-907d9cf58824>
2009-02-27 22:01:37 +0000

    メンバ宣言が欠けてたのを修正

--- PEG/trunk/code/PEG/At.php
+++ PEG/trunk/code/PEG/At.php
@@ -2,18 +2,20 @@
 
 class PEG_At extends PEG_Action
 {
+    protected $key;
+    
     /**
-     * @param int $i 配列の添え字
+     * @param int $key 配列の添え字
      * @param PEG_IParser $p
      */
-    function __construct($i, PEG_IParser $p)
+    function __construct($key, PEG_IParser $p)
     {
-        $this->i = $i;
+        $this->key = $key;
         parent::__construct($p);
     }
 
     function process($result)
     {
-        return $result[$this->i];
+        return $result[$this->key];
     }
 }
