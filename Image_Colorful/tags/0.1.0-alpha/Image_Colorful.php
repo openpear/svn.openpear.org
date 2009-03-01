13b82a72d3483971aa9ad20afa10be7323210ce2
Masahiro Saito <camelmasa@users.noreply.github.com>
2009-03-01 02:19:36 +0000

    とりあえず追加

---
+++ Image_Colorful/tags/0.1.0-alpha/Image_Colorful.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Class for colorful generator
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE:
+ *
+ * Copyright (c) 2009, Masahiro Saito
+ * All rights reserved.
+ *
+ * @package     Colorful_Generator
+ * @author      Masahiro Saito <camelmasa@gmail.com>
+ * @copyright   2009 Masahiro Saito
+ * @license     http://opensource.org/licenses/bsd-license.php New BSD License
+ * @version     0.01
+ * @link        http://camelmasa.com/
+ */
+
+
+class Colorful_Generator
+{
+
+  var $_imageheight;
+
+  var $_imagewidth;
+
+  var $_splitheight;
+
+  var $_splitwidth;
+
+  var $_flag = true;
+
+  var $_content_type = "image/gif";
+
+  function Colorful_Generator($height = '', $width = '', $split_height = 1, $split_width = 1 )
+  {
+    if(!preg_match('/^[0-9]+$/',$width)) {
+      $this->showError();
+    }
+    if(!preg_match('/^[0-9]+$/',$height)) {
+      $this->showError();
+    }
+    if(!preg_match('/^[0-9]+$/',$split_height)) {
+      $this->showError();
+    }
+    if(!preg_match('/^[0-9]+$/',$split_width)) {
+      $this->showError();
+    }
+
+    $this->_imageheight = $height; 
+    $this->_imagewidth = $width; 
+    $this->_splitheight = $split_height; 
+    $this->_splitwidth = $split_width; 
+  }
+
+  function getGenerateImage(){
+    if($this->flag == false){
+      $this->showError();
+    }
+    $image = imagecreatetruecolor($this->_imageheight, $this->_imagewidth);
+    imagecolorallocate($image, 0, 0, 0);
+    for($i=0;$i<$this->_splitheight;$i++){//height
+      for($j=0;$j<$this->_splitwidth;$j++){//width
+        $color = imagecolorallocate($image, rand(0,255), rand(0,255), rand(0,255));
+        imagefilledpolygon($image, array(floor((($this->_imagewidth / $this->_splitwidth) * ($j))), 0,
+                                         floor(($this->_imagewidth / $this->_splitwidth) * ($j + 1)), 0,
+                                         floor(($this->_imagewidth / $this->_splitwidth) * ($j + 1)),floor(($this->_imageheight / $this->_splitheight) * ($i + 1)),
+                                         floor(($this->_imagewidth / $this->_splitwidth) * ($j)), floor(($this->_imageheight / $this->_splitheight) * ($i + 1))), 4, $color);
+      }
+    }
+    header('Content-type: '.$this->_content_type);
+    imagegif($image);
+  }
+
+  function showError(){
+    $this->flag = false;
+    return false;
+  }
+
+  function Debug(){
+    $this->_content_type = "text/html";
+    echo $this->_imageheight."<br>";
+    echo $this->_imagewidth."<br>";
+    echo $this->_splitheight."<br>";
+    echo $this->_splitwidth."<br>";
+    echo $this->_flag."<br>";
+    echo $this->_content_type."<br>";
+    exit;
+  }
+
+}
---
+++ Image_Colorful/trunk/sample/sample.php
@@ -0,0 +1,6 @@
+<?php
+require_once('colorful_generator.php');
+
+$image = new Colorful_Generator(200,200,1,99);
+$image->getGenerateImage();
+?>
