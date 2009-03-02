d9911a159bbc5561e40b1114fdd5261cd95bbe81
openpear <openpear@224e63c0-5457-0410-989b-907d9cf58824>
2009-03-02 13:20:59 +0000

    [Add Tag:Release] PEG

---
+++ PEG/tags/0.9.1-beta/PEG/Anything.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * どのような文字にでもヒットするパーサ
+ *
+ */
+class PEG_Anything implements PEG_IParser
+{
+    function __construct() { }
+    function parse(PEG_IContext $context)
+    {
+        if ($context->eos()) return PEG_Failure;
+        return $context->read(1);
+    }
+}
---
+++ PEG/tags/0.9.1-beta/PEG/Ref.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * パーサ同士がお互いに依存しているときに使ったりする
+ * 例えばコンストラクタでお互いのインスタンスを要求するときなど
+ *
+ */
+class PEG_Ref implements PEG_IParser
+{
+    protected $parser;
+    
+    function is(PEG_IParser $p)
+    {
+        $this->parser = $p;
+    }
+    
+    function parse(PEG_IContext $c)
+    {
+        return $this->parser->parse($c);
+    }
+}
