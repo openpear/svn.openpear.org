9ab48838f1417be91a75dce9f66bce6892e4fa38
anatoo <anatoo.jp@gmail.com>
2009-03-02 08:57:57 +0000

    サンプル追加

--- HatenaSyntax/trunk/test/t/t.php
+++ HatenaSyntax/trunk/test/t/t.php
@@ -2,16 +2,7 @@
 include_once dirname(__FILE__) . '/lime.php';
 include_once dirname(__FILE__) . '/default.php';
 
-foreach (array('/../../code') as $path) {
-     set_include_path(dirname(__FILE__) . $path . PATH_SEPARATOR . get_include_path());
-}
-
-include_once 'HatenaSyntax.php';
-
-function token($token)
-{
-    return PEG::token($token);
-}
+include_once dirname(__FILE__) . '/../../code/HatenaSyntax.php';
 
 function context($s)
 {
