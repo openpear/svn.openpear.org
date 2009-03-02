bb464eaaac5d778d6539364f399c489438aa2c5b
camelmasa <camelmasa@224e63c0-5457-0410-989b-907d9cf58824>
2009-03-02 07:20:12 +0000

    メソッドの追加

--- Image_Colorful/trunk/sample/sample.php
+++ Image_Colorful/trunk/sample/sample.php
@@ -1,6 +1,8 @@
 <?php
 require_once('Image_Colorful.php');
 
-$image = new Image_Colorful(700,700,1,455);
-$image->getGenerateImage('jpeg');
+$image = new Image_Colorful(700,200,175,1);
+$image->setTitle('ImageColorful', 'altan.ttf', '50', '35', '70');
+$image->setAuthor('camelmasa', 'altan.ttf', '35', '400', '170');
+$image->getGenerateImage('png');
 ?>
