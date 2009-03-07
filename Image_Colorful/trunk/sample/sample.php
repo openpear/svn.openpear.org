230c8839dbe44d46449de291e50d06dcb561291b
Masahiro Saito <camelmasa@users.noreply.github.com>
2009-03-07 13:46:39 +0000

    0.1.1-stableのリリース

--- Image_Colorful/trunk/sample/sample.php
+++ Image_Colorful/trunk/sample/sample.php
@@ -5,6 +5,7 @@ require_once('./Image_Colorful.php');
 $image = new Image_Colorful(700,200,175,1);
 $image->addTexts('ImageColorful', './font/altan.ttf', '50', '35', '70');
 $image->addTexts('camelmasa', './font/altan.ttf', '35', '400', '170');
+$image->addColors(150,150,150);
 $image->getGenerateImage('png');
 $image->saveGenerateImage('a.png','png');
 ?>
