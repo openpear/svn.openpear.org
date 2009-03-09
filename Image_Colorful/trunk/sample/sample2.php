337a5e68e8b508a5b2b0761c1fd492e4d9f5adb6
camelmasa <camelmasa@224e63c0-5457-0410-989b-907d9cf58824>
2009-03-09 07:49:33 +0000

    0.1.3-stableのリリース

---
+++ Image_Colorful/trunk/sample/sample2.php
@@ -0,0 +1,7 @@
+<?php
+require_once('./Image_Colorful_Word.php');
+
+$image = new Image_Colorful_Word('ImageCalorfulWord', './font/altan.ttf', 50, array(5,5,5,5), array(255,255,255), 50, 1);
+$image->addColors(150,150,150);
+$image->getGenerateImage('png');
+?>
