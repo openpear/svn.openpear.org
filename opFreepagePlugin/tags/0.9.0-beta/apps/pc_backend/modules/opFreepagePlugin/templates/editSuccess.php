6393973d2fc7645cc26bbd92c581eb06a87689a2
uechoco <uechoco@224e63c0-5457-0410-989b-907d9cf58824>
2009-03-13 12:52:38 +0000

    tagsの使い方を間違えた。

--- opFreepagePlugin/tags/0.9.0-beta/apps/pc_backend/modules/opFreepagePlugin/templates/editSuccess.php
+++
@@ -1,7 +0,0 @@
-<?php slot('submenu') ?>
-<?php include_partial('submenu') ?>
-<?php end_slot() ?>
-
-<h2><?php echo __('フリーページの編集') ?></h2>
-
-<?php include_partial('form', array('form' => $form)) ?>
