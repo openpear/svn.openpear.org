072784207a48583b5fc5746588a02996990768f2
anatoo <anatoo.jp@gmail.com>
2009-04-12 07:04:58 +0000

    正規表現使えるようにした

---
+++ PEG/trunk/code/PEG/Preg.php
@@ -0,0 +1,20 @@
+<?php
+
+class PEG_Preg implements PEG_IParser 
+{
+    protected $pattern;
+    function __construct($pattern)
+    {
+        $this->pattern = $pattern;
+    }
+    
+    function parse(PEG_IContext $context)
+    {
+        if (preg_match($this->pattern, $context->get(), $matches, null, $context->tell())) {
+            $context->seek($context->tell() + strlen($matches[0]));
+            return $matches[0];
+        }
+        
+        return PEG::failure();
+    }
+}
