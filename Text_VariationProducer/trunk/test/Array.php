79bfe0b8cc3c4e53b6681c5c951b45c194bea9b5
hnw <hnw@users.noreply.github.com>
2009-04-29 04:23:08 +0000

    テスト追加

---
+++ Text_VariationProducer/trunk/test/Array.php
@@ -0,0 +1,15 @@
+<?php
+include_once(dirname(__FILE__) . '/t/t.php');
+
+$lime = new lime_test;
+$strings1 = new Text_VariationProducer(array('a'));
+$strings2 = new Text_VariationProducer(array('a', '[b-d]'));
+$strings3 = new Text_VariationProducer(array(''));
+$strings4 = new Text_VariationProducer(array());
+
+//--
+
+$lime->ok(iterator_to_array($strings1) === array('a'));
+$lime->ok(iterator_to_array($strings2) === array('a', 'b', 'c', 'd'));
+$lime->ok(iterator_to_array($strings3) === array(''));
+$lime->ok(iterator_to_array($strings4) === array());
