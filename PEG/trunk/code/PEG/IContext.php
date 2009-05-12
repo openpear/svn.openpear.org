b3f9dc2f1e800c942dc01a7d1a99c78e553ead52
anatoo <anatoo.jp@gmail.com>
2009-05-12 16:27:55 +0000

    PEG_IContext::readElement追加。それに伴い実装クラスにも追加。

--- PEG/trunk/code/PEG/IContext.php
+++ PEG/trunk/code/PEG/IContext.php
@@ -25,12 +25,17 @@ interface PEG_IContext
     function seek($i);
     
     /**
-     * 対象の一部を返す。その際に現在位置も変更する。
+     * 対象の一部を$i分返す。その際に$iだけ現在位置も変更する。
      *
      * @param int $i
      * @return string
      */
     function read($i);
+    
+    /**
+     * 対象の要素を一つ返す。その際に現在位置も変更する。
+     */
+    function readElement();
     
     /**
      * 読み込むべきものが無い場合trueを返す。
@@ -40,10 +45,13 @@ interface PEG_IContext
     
     /**
      * コンテキストが持つ対象全体を返す
+     * 実装クラスは例外を投げることでこれを拒否できる
+     * @return ?
      */
     function get();
     
     /**
+     * このメソッドの実装にはPEG_Cacheクラスの使用を推奨する
      * @param PEG_IParser 
      * @param int 
      * @param ? 
@@ -51,8 +59,8 @@ interface PEG_IContext
     function save(PEG_IParser $parser, $start, $end, $val);
     
     /**
+     * このメソッドの実装にはPEG_Cacheクラスの使用を推奨する
      * array(hit, array(end, val))を返す
-     * 
      * @param PEG_IParser
      * @param int
      * @return array
