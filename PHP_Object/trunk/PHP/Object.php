644629b01f7a5a33b81ab002fb6a94facce4c286
Naoyuki Kanezawa <naoyuki.kanezawa@gmail.com>
2009-05-17 03:44:02 +0000

    設定用のクラス追加

--- PHP_Object/trunk/PHP/Object.php
+++ PHP_Object/trunk/PHP/Object.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'PHP/Object/Array.php';
 require_once 'PHP/Object/Boolean.php';
+require_once 'PHP/Object/Configure.php';
 require_once 'PHP/Object/Null.php';
 require_once 'PHP/Object/Numeric/Integer.php';
 require_once 'PHP/Object/Numeric/Float.php';
@@ -11,77 +12,16 @@ require_once 'PHP/Object/String.php';
 
 class PHP_Object 
 {
-    protected $data = null;
-
-    protected $aliasMethods = array();
-
-    protected $methodsReturningSelf = array(
-        'clearstatcache',
-        'closedir',
-        'date_date_set',
-        'date_isodate_set',
-        'date_modify',
-        'date_time_set',
-        'date_timezone_set',
-        'dba_close',
-        'debug_print_backtrace',
-        'debug_zval_dump',
-        'define_syslog_variables',
-        'flush',
-        'header',
-        'ini_restore',
-        'libxml_clear_errors',
-        'libxml_set_streams_context',
-        'mt_srand',
-        'mysqli_free_result',
-        'mysqli_set_local_infile_default',
-        'mysqli_stmt_data_seek',
-        'mysqli_stmt_free_result',
-        'ncurses_bkgdset',
-        'ncurses_filter',
-        'ncurses_getmaxyx',
-        'ncurses_getyx',
-        'ncurses_init',
-        'ncurses_noqiflush',
-        'ncurses_qiflush',
-        'ncurses_timeout',
-        'ncurses_update_panels',
-        'ncurses_use_env',
-        'ob_clean',
-        'ob_flush',
-        'ob_implicit_flush',
-        'openssl_free_key',
-        'openssl_pkey_free',
-        'openssl_x','_free',
-        'parse_str',
-        'passthru',
-        'pcntl_exec',
-        'register_shutdown_function',
-        'restore_include_path',
-        'rewinddir',
-        'session_set_cookie_params',
-        'session_unset',
-        'session_write_close',
-        'set_time_limit',
-        'shmop_close',
-        'socket_clear_error',
-        'socket_close',
-        'spl_autoload',
-        'spl_autoload_call',
-        'srand',
-        'stream_bucket_append',
-        'stream_bucket_prepend',
-        'unregister_tick_function',
-        'usleep',
-        'var_dump',
-        'zip_close',
-    );
+    protected $data;
 
     protected $argOffsets = array();
 
+    public $configure;
+
     protected function __construct(&$data)
     {
-        $this->data =& $data;
+        $this->configure = PHP_Object_Configure::getInstance(get_class($this));
+        $this->data = &$data;
     }
 
     protected function __toString() 
@@ -95,12 +35,12 @@ class PHP_Object
         $name = $this->aliasMethod($name);
 
         if (function_exists($name)) {
-            if (($offset = $this->argOffset($name)) !== null) {
+            if (($offset = $this->argOffset($name)->val()) !== null) {
                 array_splice($args, $offset, 0, array(&$this->data));
             }
             $result = call_user_func_array($name, $args);
 
-            if (in_array($name, $this->methodsReturningSelf)) {
+            if ($this->isMethodReturningSelf($name)->val()) {
                 return $this;
             } else {
                 return self::factory($result);
@@ -110,7 +50,6 @@ class PHP_Object
         if (($result = $this->callLanguageConstruct($name, $args)) != false) {
             return $result;
         }
-        
     }
 
     protected function __get($name)
@@ -152,16 +91,9 @@ class PHP_Object
         }
     }
 
-    protected function aliasMethod($name) {
-        if (!function_exists($name)) {
-            $aliases = str_replace('*', $name, $this->aliasMethods);
-            foreach ($aliases as $alias) {
-                if (function_exists($alias)) {
-                    return $alias;
-                }
-            }
-        }
-        return $name;
+    protected function aliasMethod($method)
+    {
+        return $this->configure->aliasMethod($method);
     }
 
     protected function callLanguageConstruct($name, $args)
@@ -220,9 +152,13 @@ class PHP_Object
         return $data;
     }
 
-    public function argOffset($name) 
+    public function argOffset($method) 
     {
-        return array_key_exists($name, $this->argOffsets) ? $this->argOffsets[$name] : null;
+        return self::factory($this->configure->argOffset($method));
+    }
+
+    public function isMethodReturningSelf($method) {
+        return self::factory($this->configure->isMethodReturningSelf($method));
     }
 
     public function val() 
@@ -237,7 +173,7 @@ class PHP_Object
         $name = $this->aliasMethod(array_shift($args));
 
         if (function_exists($name)) {
-            if (($offset = $this->argOffset($name)) !== null) {
+            if (($offset = $this->argOffset($name)->val()) !== null) {
                 array_splice($args, $offset, 0, array(&$this->data));
             }
             call_user_func_array($name, $args);
