aab62d8645af82220477e246b899dba7c2a35011
localdisk <smoochynet@gmail.com>
2009-05-20 03:04:26 +0000

    コメント書き直し

--- Services_Backlog/trunk/Services/Backlog.php
+++ Services_Backlog/trunk/Services/Backlog.php
@@ -1,4 +1,24 @@
 <?php
+/**
+ * PHP Client for Backlog API
+ *
+ * PHP versions 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ *
+ * @category   Services
+ * @package    Services_Backlog
+ * @author     devworks <smoochynet@gmail.com>
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    0.1.0
+ * @see        http://www.backlog.jp/api/
+ * @uses       XML_RPC
+ */
 require_once 'XML/RPC.php';
 require_once 'Services/Backlog/Exception.php';
 /*
@@ -6,12 +26,9 @@ require_once 'Services/Backlog/Exception.php';
  *
  * PHP versions 5
  *
- * @category   Services
  * @package    Services_Backlog
  * @author     devworks <smoochynet@gmail.com>
- * @access     public
  * @version    Release: 0.1.0
- * @uses       XML_RPC
  */
 class Services_Backlog {
     /**
--- Services_Backlog/trunk/Services/Backlog/Exception.php
+++ Services_Backlog/trunk/Services/Backlog/Exception.php
@@ -1,7 +1,19 @@
 <?php
-/*
- * Services_Backlogの例外クラス
- * PHP5以上対象
+/**
+ * Exception Class for Services_Backlog
+ *
+ * PHP versions 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ */
+
+/**
+ * Services_Backlog_Exception
  *
  * @package    Services_Backlog
  * @author     devworks <smoochynet@gmail.com>
