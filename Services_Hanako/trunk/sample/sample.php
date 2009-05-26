731f72d1ac087491b91d32dbb9f0d31ec7f933ee
Hideyuki Shimooka <shimooka@doyouphp.jp>
2009-05-26 15:40:48 +0000

    - changed args of constructor - added test

--- Services_Hanako/trunk/sample/sample.php
+++ Services_Hanako/trunk/sample/sample.php
@@ -2,5 +2,5 @@
 ini_set("include_path", dirname(__FILE__)."/../" . PATH_SEPARATOR . ini_get("include_path"));
 require_once "Services/Hanako.php";
 
-$meter = new Services_Hanako('03', '50810100');
+$meter = new Services_Hanako(new HTTP_Request2(), '03', '50810100');
 var_dump($meter->now());
