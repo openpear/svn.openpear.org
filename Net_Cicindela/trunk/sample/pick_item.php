2337e36feae0881c65e28306cf9ae6cb00713264
TANAKA Koichi <mugeso@users.noreply.github.com>
2009-06-11 08:07:46 +0000

    Net_Cicindela初期コミット

---
+++ Net_Cicindela/trunk/sample/pick_item.php
@@ -0,0 +1,10 @@
+<?php
+ini_set("include_path",  dirname(__FILE__) . "/../src".PATH_SEPARATOR.ini_get("include_path"));
+require_once 'Net/Cicindela.php';
+
+$config = include('./config.php');
+$cicindela = new Net_Cicindela($config['base_url']);
+
+$clipRecommender = $cicindela->getDataset('clip_simple');
+$clipRecommender->pickItem(123, 5122);
+?>
