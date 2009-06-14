147e55bb9148ff48646f9c094f605372902c934e
anatoo <anatoo@224e63c0-5457-0410-989b-907d9cf58824>
2009-06-14 16:57:43 +0000

    PEG_RefにgetRef()とis()を追加。テスト追加。

--- PEG/trunk/test/Ref.php
+++ PEG/trunk/test/Ref.php
@@ -6,3 +6,12 @@ $t = new lime_test;
 $p = PEG::ref($p_ref);
 $p_ref = PEG::token('hoge');
 $t->is($p->parse(PEG::context('hoge')), 'hoge');
+
+$p = PEG::ref($_);
+$ref =& $p->getRef();
+$ref = PEG::token('hoge');
+$t->is($p->parse(PEG::context('hoge')), 'hoge');
+
+$p = PEG::ref($_);
+$p->is(PEG::token('hoge'));
+$t->is($p->parse(PEG::context('hoge')), 'hoge');
