8e73464c05bf30de94be32db2fbbb1d951963f07
anatoo <anatoo@224e63c0-5457-0410-989b-907d9cf58824>
2009-08-06 05:30:49 +0000

    コメント追加

--- Image_GifColorConverter/trunk/sample/sample.php
+++ Image_GifColorConverter/trunk/sample/sample.php
@@ -1,7 +1,12 @@
 <?php
 include_once dirname(__FILE__) . '/../lib/Image/GifColorConverter.php';
 
+/**
+ * このサンプルを実行すると、0_converted.gf, 1_converted.gifというファイルが生成されます
+ */
+
 $converter = new GifColorConverter;
+
 $converter->put('0.gif', '0_converted.gif', array(0x000000 => 0xff0000, 0xffffff => 0xcccccc)); 
 
 $converter->put('1.gif', '1_converted.gif', array(0x000000 => 0x0099ff)); 
