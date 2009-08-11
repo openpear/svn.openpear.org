f2c22ee27b57e26d0d6b6fb0bf0aa1d087e97849
anatoo <anatoo.jp@gmail.com>
2009-08-11 02:52:06 +0000

    PEG::tokenのテスト追加

--- PEG/trunk/test/Token.php
+++ PEG/trunk/test/Token.php
@@ -8,7 +8,8 @@ $context = PEG::context('hogehoge');
 $t->is($token->parse($context), 'hoge');
 $t->is($context->tell(), 4);
 $t->is($token->parse($context), 'hoge');
-
 $t->is($token->parse($context), PEG::failure());
 
 $t->is(PEG::token('hoge', false)->parse(PEG::context('Hoge')), 'Hoge');
+$t->is(PEG::token('hoge', false)->parse(PEG::context('hoge')), 'hoge');
+$t->is(PEG::token('hoge', false)->parse(PEG::context('fuga')), PEG::failure());
