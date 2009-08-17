83e44331afb4ee014914bf15282bd15a922dbb69
cocoiti <cocoiti@224e63c0-5457-0410-989b-907d9cf58824>
2009-08-17 17:15:31 +0000

    add pecl benchmark

--- Net_TokyoTyrant/trunk/tests/benchmark/benchmark_pecl.php
+++ Net_TokyoTyrant/trunk/tests/benchmark/benchmark_pecl.php
@@ -1,15 +1,14 @@
 <?php
-require_once dirname(dirname(dirname(__FILE__))) . '/Net/TokyoTyrant.php';
+//require_once dirname(dirname(__FILE__)) . '/Net/TokyoTyrant.php';
 require_once 'Benchmark/Timer.php';
 $timer = new Benchmark_Timer();
 $timer->start();
 
-$tt = new Net_TokyoTyrant();
+$tt = new TokyoTyrant();
 $timer->setMarker('create');
 $tt->connect('localhost', 1978);
 $timer->setMarker('connect');
-assert($tt->vanish() === true);
-$timer->setMarker('vanish');
+
 
 $data = 'aaaaaaa';
 for($i = 0;$i <= 10000 ;$i++){
@@ -26,6 +25,5 @@ for($i = 0;$i <= 10000 ;$i++){
 
 $timer->setMarker('get');
 
-$tt->close();
 $timer->setMarker('close');
 $timer->display();
