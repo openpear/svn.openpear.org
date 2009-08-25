9c63cb5ffd036822c0a8f8558b86c0a9ba20e2d1
FreeBSE <FreeBSE@224e63c0-5457-0410-989b-907d9cf58824>
2009-08-25 11:33:18 +0000

    ファイルの中身を16進数で表示するメソッドを追加 ファイルの中身を2進数で表示するメソッドを追加

--- PHP_PowerToys/trunk/sample/sample.php
+++ PHP_PowerToys/trunk/sample/sample.php
@@ -3,54 +3,57 @@ require_once('../PowerToys.php');
 $pt = new PHP_Powertoys();
 
 //PEAR系
-$obj = $pt->getPearI('Net_POP3');
-$obj = $pt->getPear('Net/POP3.php');
+//$obj = $pt->getPearI('Net_POP3');
+//$obj = $pt->getPear('Net/POP3.php');
 
 //変数/配列系
-$a['a'] = 'a';
-$a['b'] = 'b';
-$a['c'] = 'c';
+//$a['a'] = 'a';
+//$a['b'] = 'b';
+//$a['c'] = 'c';
 
-$a = $pt->arrayKeyReplace($a, 'b', 'd');
+//$a = $pt->arrayKeyReplace($a, 'b', 'd');
 //実際に保存されちゃうのでコメントアウト
 //$pt->save_var_dump($a, 'var_dump.txt');
-$pt->print_r_ex($a);
+//$pt->print_r_ex($a);
 
 //UTF-8系
-$bom = $pt->checkBom('sample.txt');
-if($bom){
-	echo 'BOM付きです';
-}
+//$bom = $pt->checkBom('sample.txt');
+//if($bom){
+//	echo 'BOM付きです';
+//}
 
-$str = file_get_contents('sample.txt');
-$str = $pt->removeBom($str);
-$bom = $pt->checkBom($str);
-if(!$bom){
-	echo 'BOMなしです';
-}
+//$str = file_get_contents('sample.txt');
+//$str = $pt->removeBom($str);
+//$bom = $pt->checkBom($str);
+//if(!$bom){
+//	echo 'BOMなしです';
+//}
 
-$str = file_get_contents('nobom.txt');
-$str = $pt->addBom($str);
-$bom = $pt->checkBom($str);
-if($bom){
-	echo 'BOMありです';
-}
+//$str = file_get_contents('nobom.txt');
+//$str = $pt->addBom($str);
+//$bom = $pt->checkBom($str);
+//if($bom){
+//	echo 'BOMありです';
+//}
 
 //画像系
-$img = $pt->iopen(dirname(dirname(__FILE__)) . '/sample/sample.jpg');
-$img = $pt->iopen(dirname(dirname(__FILE__)) . '/sample/sample.png');
-$img = $pt->iopen(dirname(dirname(__FILE__)) . '/sample/sample.gif');
-$img = $pt->iopen(dirname(dirname(__FILE__)) . '/sample/sample.bmp');
+//$img = $pt->iopen(dirname(dirname(__FILE__)) . '/sample/sample.jpg');
+//$img = $pt->iopen(dirname(dirname(__FILE__)) . '/sample/sample.png');
+//$img = $pt->iopen(dirname(dirname(__FILE__)) . '/sample/sample.gif');
+//$img = $pt->iopen(dirname(dirname(__FILE__)) . '/sample/sample.bmp');
 
 //オブジェクト系
-$pt2 = $pt->objectClone($pt);
+//$pt2 = $pt->objectClone($pt);
 
 //ファイル取得
 //echo $pt2->file_get_contents_ex('http://openpear.org/');
 
-$array = array('abcdefg', 'abcd' , 'def', 'hij', 'klm');
-$result = $pt2->in_array_ex('.*d.*', $array);
-echo $result; //結果は恐らく3
-$ini = $pt2->iniParser('sample.ini');
-print_r($ini);
+//$array = array('abcdefg', 'abcd' , 'def', 'hij', 'klm');
+//$result = $pt2->in_array_ex('.*d.*', $array);
+//echo $result; //結果は恐らく3
+//$ini = $pt2->iniParser('sample.ini');
+//print_r($ini);
+
+//$data = $pt->hexFromFile('');
+//echo $data;
 ?>
