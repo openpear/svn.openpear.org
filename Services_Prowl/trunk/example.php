8f7f9c8bcfd516ba13daa5628c4b56ae730d6e49
halt <halt@224e63c0-5457-0410-989b-907d9cf58824>
2009-09-25 15:25:25 +0000

    コードをsrcディレクトリに移動

--- Services_Prowl/trunk/example.php
+++ Services_Prowl/trunk/example.php
@@ -7,6 +7,8 @@
 require_once 'Services/Prowl.php';
 require_once 'config.php';
 
+// $api_key = '';
+
 $prowl = new Services_Prowl($api_key);
 $prowl->push('Test message from Services_Prowl at ' . date('Y-m-d H:i:s'));
 
