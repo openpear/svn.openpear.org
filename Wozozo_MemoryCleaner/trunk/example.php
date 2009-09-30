6ee69a9bb9ba117dc6f0b2301b0ddc3ae6c1324b
halt <halt@224e63c0-5457-0410-989b-907d9cf58824>
2009-09-30 13:24:41 +0000

    initial import

---
+++ Wozozo_MemoryCleaner/trunk/example.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ *
+ */
+
+require_once './src/Wozozo/MemoryClean.php';
+
+// example
+$data = array();
+foreach (range(0, strlen(__FILE__)) as $no_use) {
+    $data[] = file_get_contents(__FILE__);
+}
+
+echo 'before memory usage:' . memory_get_usage() . 'byte' . PHP_EOL;
+
+$wozozo = new Wozozo_MemoryCleaner();
+
+echo 'after memory usage:' . memory_get_usage() . 'byte' . PHP_EOL;
+
