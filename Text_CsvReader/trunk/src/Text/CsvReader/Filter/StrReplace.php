d3977c4afc6959eb7f5b741cffc97510b78cd293
hnw <hnw@users.noreply.github.com>
2009-10-06 14:44:41 +0000

    PregReplace Filterを追加

--- Text_CsvReader/trunk/src/Text/CsvReader/Filter/StrReplace.php
+++ Text_CsvReader/trunk/src/Text/CsvReader/Filter/StrReplace.php
@@ -2,6 +2,8 @@
 class Text_CsvReader_Filter_StrReplace extends Text_CsvReader_Mapper
 {
   protected $requiredOptions = array('from', 'to');
+  protected $targetOptions = array('from', 'to');
+
   protected function map($value, $column_index)
   {
     return str_replace($this->getOption('from', $column_index),
