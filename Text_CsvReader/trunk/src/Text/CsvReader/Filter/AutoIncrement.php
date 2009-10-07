24d7e0fdaf71e60546535c8adf30291467d5ff2c
hnw <hnw@users.noreply.github.com>
2009-10-07 04:55:33 +0000

    rewindしたらAutoIncrement Filterのカウンターをリセットするように

--- Text_CsvReader/trunk/src/Text/CsvReader/Filter/AutoIncrement.php
+++ Text_CsvReader/trunk/src/Text/CsvReader/Filter/AutoIncrement.php
@@ -4,6 +4,11 @@ class Text_CsvReader_Filter_AutoIncrement extends Text_CsvReader_Mapper
   protected $requiredOptions = array('column');
   protected $counter = 0;
 
+  protected function initialize()
+  {
+    $this->counter = 0;
+  }
+
   protected function mapAll($values)
   {
     $values[$this->getOption('column')] = ++$this->counter;
