e6018860cb9390d434a8ca9a7bea56da9d00a010
hnw <hnw@224e63c0-5457-0410-989b-907d9cf58824>
2009-10-07 15:20:09 +0000

    Grepper Regexを修正、テスト追加

--- Text_CsvReader/trunk/src/Text/CsvReader/Grepper/Regex.php
+++ Text_CsvReader/trunk/src/Text/CsvReader/Grepper/Regex.php
@@ -6,7 +6,7 @@ class Text_CsvReader_Grepper_Regex extends Text_CsvReader_Grepper
 
   protected function accept($value, $column_index) {
     $pattern = $this->getOption('pattern');
-    if (isset($pattern[$column_index])) {
+    if (!empty($pattern[$column_index])) {
       return (boolean)preg_match($pattern[$column_index], $value);
     }
     return true;
