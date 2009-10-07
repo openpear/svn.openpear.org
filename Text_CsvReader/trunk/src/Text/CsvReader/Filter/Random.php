f2318c1291759faa91bdedee337b2829da707c0a
hnw <hnw@users.noreply.github.com>
2009-10-07 14:42:08 +0000

    Random Filter: min/maxオプションを廃止、rangeオプションを追加

--- Text_CsvReader/trunk/src/Text/CsvReader/Filter/Random.php
+++ Text_CsvReader/trunk/src/Text/CsvReader/Filter/Random.php
@@ -1,10 +1,9 @@
 <?php
 class Text_CsvReader_Filter_Random extends Text_CsvReader_Mapper
 {
-  protected $options = array('min' => array(),
-                             'max' => array(),
+  protected $options = array('range' => array(),
                              'choice' => array());
-  protected $targetOptions = array('min', 'max', 'choice');
+  protected $targetOptions = array('range', 'choice');
 
   protected function map($value, $column_index)
   {
@@ -12,8 +11,8 @@ class Text_CsvReader_Filter_Random extends Text_CsvReader_Mapper
       $choice = $this->getOption('choice', $column_index);
       return $choice[mt_rand(0, sizeof($choice)-1)];
     } else {
-      return mt_rand($this->getOption('min', $column_index),
-                     $this->getOption('max', $column_index));
+      $range = $this->getOption('range', $column_index);
+      return mt_rand($range[0], $range[1]);
     }
   }
 }
