5c263520466aea99758d22d88c1000db87322f57
TANAKA Koichi <mugeso@users.noreply.github.com>
2009-10-07 09:58:51 +0000

    絵がないとさみしいよね

--- Wozozo_Saigoppe/trunk/Wozozo/Saigoppe.php
+++ Wozozo_Saigoppe/trunk/Wozozo/Saigoppe.php
@@ -2,21 +2,34 @@
 class Wozozo_Saigoppe
 {
     private $onomatopoeia = 'プゥゥゥゥゥ';
-
-    public function __construct($onomatopoeia=null)
+    private $ascii_art = 'or2=3';
+    private $enable_aa;
+    
+    public function __construct($onomatopoeia=null, $enable_aa = true)
     {
         $onomatopoeia and $this->onomatopoeia = $onomatopoeia;
+        $this->enable_aa = $enable_aa;
         register_shutdown_function(array($this, 'poot'));
     }
 
     public function poot()
     {
-        echo $this->onomatopoeia, PHP_EOL;
+        echo empty($this->ascii_art) || !$this->enable_aa ? '' : $this->ascii_art . ' ' ,$this->onomatopoeia, PHP_EOL;
     }
 
     public function setOnomatopoeia($onomatopoeia)
     {
         $this->onomatopoeia = $onomatopoeia;
+    }
+
+    public function setAsciiArt($ascii_art)
+    {
+        $this->ascii_art = $ascii_art;
+    }
+
+    public function enableAsciiArt($enable=true)
+    {
+        $this->enable_aa = $enable;
     }
 }
 ?>
--- Wozozo_Saigoppe/trunk/sample/sample.php
+++ Wozozo_Saigoppe/trunk/sample/sample.php
@@ -2,5 +2,6 @@
 include_once(dirname(__FILE__) . '/../Wozozo/Saigoppe.php');
 new Wozozo_Saigoppe();
 new Wozozo_Saigoppe('ブリブリ');
+new Wozozo_Saigoppe('poot!', false);
 ?>
 EOF
