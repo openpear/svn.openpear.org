69682f02de2e43b411eb0d8d513ee9223cdd19f3
hnw <hnw@224e63c0-5457-0410-989b-907d9cf58824>
2009-10-07 11:57:50 +0000

    Random Filterに配列から1個を選ぶ機能を追加

--- Text_CsvReader/trunk/src/Text/CsvReader/Filter/Random.php
+++ Text_CsvReader/trunk/src/Text/CsvReader/Filter/Random.php
@@ -1,12 +1,19 @@
 <?php
 class Text_CsvReader_Filter_Random extends Text_CsvReader_Mapper
 {
-  protected $requiredOptions = array('min', 'max');
-  protected $targetOptions = array('min', 'max');
+  protected $options = array('min' => array(),
+                             'max' => array(),
+                             'choice' => array());
+  protected $targetOptions = array('min', 'max', 'choice');
 
   protected function map($value, $column_index)
   {
-    return mt_rand($this->getOption('min', $column_index),
-                   $this->getOption('max', $column_index));
+    if ($this->hasOption('choice', $column_index)) {
+      $choice = $this->getOption('choice', $column_index);
+      return $choice[mt_rand(0, sizeof($choice)-1)];
+    } else {
+      return mt_rand($this->getOption('min', $column_index),
+                     $this->getOption('max', $column_index));
+    }
   }
 }
