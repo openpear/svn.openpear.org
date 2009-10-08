94794c7a3f3c84dd7e7ac8fd8f1fe1765a28d511
hnw <hnw@users.noreply.github.com>
2009-10-08 04:44:40 +0000

    Unique Grepperで複数カラムを指定された場合に重複の判定を間違えることがあったのを修正

--- Text_CsvReader/trunk/src/Text/CsvReader/Grepper/Unique.php
+++ Text_CsvReader/trunk/src/Text/CsvReader/Grepper/Unique.php
@@ -6,7 +6,7 @@ class Text_CsvReader_Grepper_Unique extends Text_CsvReader_Grepper
     $indexes = $this->getTargetColumns($values);
     $key = "";
     foreach ($indexes as $index) {
-      $key .= $values[$index];
+      $key .= $values[$index]."\0";
     }
     if (isset($this->already_exists[$key])) {
       return false;
