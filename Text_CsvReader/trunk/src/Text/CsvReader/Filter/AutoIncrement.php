aca5cd375bb50fd81c782ef97314ea68fe3631a8
hnw <hnw@224e63c0-5457-0410-989b-907d9cf58824>
2009-10-08 04:29:28 +0000

    AutoIncrement Filterで、後続のFilterがcurrentを何度も呼んだ場合に毎回異なる値を返してしまうバグを修正

--- Text_CsvReader/trunk/src/Text/CsvReader/Filter/AutoIncrement.php
+++ Text_CsvReader/trunk/src/Text/CsvReader/Filter/AutoIncrement.php
@@ -1,17 +1,24 @@
 <?php
 class Text_CsvReader_Filter_AutoIncrement extends Text_CsvReader_Mapper
 {
+  // target に統一したいなー。
   protected $requiredOptions = array('column');
   protected $counter = 0;
 
-  protected function initialize()
+  public function rewind()
   {
-    $this->counter = 0;
+    $this->counter = 1;
+    return parent::rewind();
+  }
+  public function next()
+  {
+    $this->counter++;
+    return parent::next();
   }
 
   protected function mapAll($values)
   {
-    $values[$this->getOption('column')] = ++$this->counter;
+    $values[$this->getOption('column')] = $this->counter;
     return $values;
   }
 }
