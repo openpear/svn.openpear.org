06b0bfaa6ef7cfcb261c46217931c1ca86839867
hnw <hnw@224e63c0-5457-0410-989b-907d9cf58824>
2009-10-08 04:30:05 +0000

    Unique Grepper を整理

--- Text_CsvReader/trunk/src/Text/CsvReader/Grepper/Unique.php
+++ Text_CsvReader/trunk/src/Text/CsvReader/Grepper/Unique.php
@@ -3,13 +3,8 @@ class Text_CsvReader_Grepper_Unique extends Text_CsvReader_Grepper
 {
   protected $already_exists = array();
   protected function acceptAll($values) {
-    if ($this->hasOption('target') && is_array($this->getOption('target'))) {
-      $indexes = $this->getOption('target');
-    } else {
-      $indexes = array_keys($values);
-    }
+    $indexes = $this->getTargetColumns($values);
     $key = "";
-    $values = $this->current();
     foreach ($indexes as $index) {
       $key .= $values[$index];
     }
