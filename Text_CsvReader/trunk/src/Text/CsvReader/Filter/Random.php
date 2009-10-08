5721a01e1e8b1460d1eda6cea436705b64389ff3
hnw <hnw@users.noreply.github.com>
2009-10-08 08:24:27 +0000

    Random Filterの後ろにGrepperがある問題を修正

--- Text_CsvReader/trunk/src/Text/CsvReader/Filter/Random.php
+++ Text_CsvReader/trunk/src/Text/CsvReader/Filter/Random.php
@@ -4,15 +4,34 @@ class Text_CsvReader_Filter_Random extends Text_CsvReader_Mapper
   protected $options = array('range' => array(),
                              'choice' => array());
   protected $targetOptions = array('range', 'choice');
+  protected $randomValues = array();
 
+  public function rewind()
+  {
+    $this->fetchRandomValue();
+    return parent::rewind();
+  }
+  public function next()
+  {
+    $this->fetchRandomValue();
+    return parent::next();
+  }
+  protected function fetchRandomValue()
+  {
+    $column_indexes = $this->getTargetColumns($values);
+    foreach ($column_indexes as $column_index) {
+      if ($this->hasOption('choice', $column_index)) {
+        $choice = $this->getOption('choice', $column_index);
+        $value = $choice[mt_rand(0, sizeof($choice)-1)];
+      } else {
+        $range = $this->getOption('range', $column_index);
+        $value = mt_rand($range[0], $range[1]);
+      }
+      $this->randomValues[$column_index] = $value;
+    }
+  }
   protected function map($value, $column_index)
   {
-    if ($this->hasOption('choice', $column_index)) {
-      $choice = $this->getOption('choice', $column_index);
-      return $choice[mt_rand(0, sizeof($choice)-1)];
-    } else {
-      $range = $this->getOption('range', $column_index);
-      return mt_rand($range[0], $range[1]);
-    }
+    return $this->randomValues[$column_index];
   }
 }
