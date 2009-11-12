6601b156fcc64a385a9b59287c109533b1f39206
takada-at <takada-at@users.noreply.github.com>
2009-11-12 02:24:13 +0000

    pear-package

---
+++ PictgramConverter/trunk/HTML_PictgramConverter/sample.php
@@ -0,0 +1,10 @@
+<?php
+ini_set("include_path", dirname(__FILE__)."/src/" . PATH_SEPARATOR . ini_get("include_path"));
+require_once "HTML/PictgramConverter.php";
+
+// Test
+$obj = new HTML_PictgramConverter();
+
+
+
+?>
