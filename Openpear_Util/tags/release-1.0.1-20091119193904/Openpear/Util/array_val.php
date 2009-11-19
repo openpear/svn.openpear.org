ca66d930c3d1be13cfb40e8601f05571dfeeb03c
openpear <openpear@224e63c0-5457-0410-989b-907d9cf58824>
2009-11-19 10:39:04 +0000

    [Package Release] Openpear_Util

---
+++ Openpear_Util/tags/release-1.0.1-20091119193904/Openpear/Util/array_val.php
@@ -0,0 +1,32 @@
+<?php
+/**
+  * array_val
+  *
+  * PHP version 5
+  *
+  * LICENSE: This source file is subject to version 3.0 of the PHP license
+  * that is available through the world-wide-web at the following URI:
+  * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+  * the PHP License and are unable to obtain it through the web, please
+  * send a note to license@php.net so we can mail you a copy immediately.
+  *
+  * @category   Utils
+  * @package    Openpear_Util
+  * @author     KOYAMA Tetsuji <koyama@hoge.org>
+  * @copyright  2008-2009 KOYAMA Tetsuji
+  * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+  * @version    svn: $Id$
+  * @link       http://openpear.org/package/Openpear_Util
+  * @since      File available since Release 0.1
+  */
+
+if (!function_exists('array_val')) {
+    function array_val(&$data, $key, $default = null) {
+        if (!is_array($data) && !($data instanceof ArrayAccess)) {
+            return $default;
+        }
+        return isset($data[$key])? $data[$key]: $default;
+    }
+}
+
+?>
