125ce2099007f200bf696f4e6ed9ab850061716a
FreeBSE <FreeBSE@224e63c0-5457-0410-989b-907d9cf58824>
2009-12-25 09:56:32 +0000

    特定のデータを取得するメソッドを削除 一気にデータを取得出来るようにした 少しリファクタ

--- Services_WeatherUnderground/trunk/WeatherUnderground.php
+++ Services_WeatherUnderground/trunk/WeatherUnderground.php
@@ -4,7 +4,7 @@ define('WG_API_AP', 'http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/ind
 
 class Services_WeatherUnderground{
 
-        public $weather = null;
+	public $weather = null;
 
 	/**
 	 *コンストラクタ
@@ -13,28 +13,30 @@ class Services_WeatherUnderground{
 	 * プロパティに代入
 	 */
 	public function __construct($query){
-		$this->weather = $this->getWG($query);
+		$data = $this->getWeather($query);
+		$this->weather = $this->toArray($data);
 	}
 	
 	/**
-	 * APIを叩くところ
+	 * APIを叩く
 	 * @param $query string 今のところは
-	 * Unserializedの結果を返す
-	 * @return array
-	 * TODO まだまだリファクタ
+	 * @return XML
 	 */
-	private final function getWG($query){
+	private final function getWeather($query){
 		require_once 'HTTP/Client.php';
-                require_once 'XML/Unserializer.php';
-                $xml = new XML_Unserializer();
-                $xml->setOption('parseAttributes',true);
 		$client = new HTTP_Client();
-//                echo $this->makeUrl($query);
 		$client->get($this->makeUrl($query));
                 $response = $client->currentResponse();
                 $response['body'] = mb_convert_encoding($response['body'], 'UTF-8', 'auto');
-                $xml->unserialize($response['body']);
-                return $xml->getUnserializedData();
+                return $response['body'];
+	}
+
+	private final function toArray($data){
+	    require_once 'XML/Unserializer.php';
+	    $xml = new XML_Unserializer();
+	    $xml->setOption('parseAttributes',true);
+	    $xml->unserialize($data);
+	    return $xml->getUnserializedData();
 	}
 	
 	/**
@@ -45,44 +47,8 @@ class Services_WeatherUnderground{
 		return sprintf('%s?query=%s', WG_API_AP, $query);
 	}
 	
-	/**
-	 * 温度の取得 摂氏と華氏
-	 * @return string
-	 */
-	public function getTemperature(){
-		return $this->weather['temperature_string'];
-	}
-	
-	/**
-	 * 気圧の取得
-	 * @return int
-	 */
-	public function getPressure(){
-		return $this->weather['pressure_mb'];
-	} 
-	
-	/**
-	 * 風向の取得
-	 * @return string
-	 */
-	public function getWindDir(){
-		return $this->weather['wind_dir'];
-	}
-	
-	/**
-	 * 風速の取得
-	 * @return int
-	 */
-	public function getWindMph(){
-		return $this->weather['wind_mph'];
-	}
-	
-	/**
-	 * 現在の風の全情報を返す
-	 * @return string
-	 */
-	public function getWindString(){
-		return $this->weather['wind_string'];
+	public function getWeatherData(){
+	    return $this->weather;
 	}
 }
 ?>
