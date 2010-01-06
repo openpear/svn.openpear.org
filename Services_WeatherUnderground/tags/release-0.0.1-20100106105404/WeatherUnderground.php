40b78b2352b33452707ef6585bf7f8a340144499
openpear <openpear@224e63c0-5457-0410-989b-907d9cf58824>
2010-01-06 01:54:04 +0000

    [Package Release] Services_WeatherUnderground

---
+++ Services_WeatherUnderground/tags/release-0.0.1-20100106105404/WeatherUnderground.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ *  Services_WeatherUnderground 0.0.1
+ *
+ *  @author	    FreeBSE <freebse@live.jp> <http://panasocli.cc/wordpress>
+ *  @package	Services_WeatherUnderground
+ *  @version	Services_WeatherUnderground v 0.0.1 2009/01/06
+ *
+ */
+
+//API URL
+define('WG_API_AP', 'http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml');
+
+//エラーコード
+define('CITY_NOT_FOUND', 0x01);
+
+interface WeatherUnderground{
+    public function getWeatherData();
+}
+
+class Services_WeatherUnderground implements WeatherUnderground {
+
+	public $weather = null;
+
+	/**
+	 *コンストラクタ
+	 *
+	 * @param $query string 今のところは
+	 * プロパティに代入
+	 */
+	public function __construct($query){
+		$data = $this->getWeather($query);
+		$this->weather = $this->toArray($data);
+	}
+	
+	/**
+	 * APIを叩く
+	 * @param $query string 今のところは
+	 * @return XML
+	 */
+	private final function getWeather($query){
+		require_once 'HTTP/Client.php';
+		$client = new HTTP_Client();
+		$client->get($this->makeUrl($query));
+                $response = $client->currentResponse();
+                $response['body'] = mb_convert_encoding($response['body'], 'UTF-8', 'auto');
+                return $response['body'];
+	}
+
+	/**
+	 * XMLを配列に変換する
+	 *
+	 * @param XML $data
+	 * @return Array
+	 */
+	private final function toArray($data){
+	    require_once 'XML/Unserializer.php';
+	    $xml = new XML_Unserializer();
+	    $xml->setOption('parseAttributes',true);
+	    $xml->unserialize($data);
+	    return $xml->getUnserializedData();
+	}
+	
+	/**
+	 * WeatherUndergroundb API REST URLの生成
+	 * @return string
+	 */
+	private final function makeUrl($query){
+		return sprintf('%s?query=%s', WG_API_AP, $query);
+	}
+
+	/**
+	 * 天気情報を取得する
+	 * @return Array
+	 */
+	public function getWeatherData(){
+	    if(!$this->weather['station_id']){
+		return CITY_NOT_FOUND;
+	    }
+	    return $this->weather;
+	}
+}
+?>
---
+++ Services_WeatherUnderground/tags/release-0.0.1-20100106105404/WeatherUnderground/sample.php
@@ -0,0 +1,20 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require_once('../WeatherUnderground.php');
+$wg = new Services_WeatherUnderground('Osaka');
+if($wg->getWeatherData() === CITY_NOT_FOUND){
+    echo sprintf('指定された観測地は見つかりませんでした「Error Code 0x%02X」', $wg->getWeatherData());
+}else{
+    print_r($wg->getWeatherData());
+}
+
+/*
+foreach($wg->weather as $key => $val){
+    echo $key , " = " , $val."<br>";
+};
+*/
+
+?>
