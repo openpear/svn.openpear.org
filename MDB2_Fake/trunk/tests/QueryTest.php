9319d9aeef0303e4aa1ddbbf216726e5b101ca02
ike tohru <rokujyouhitomajp@gmail.com>
2010-01-07 07:20:14 +0000

    ディレクトリ構成の変更

--- MDB2_Fake/trunk/tests/QueryTest.php
+++ MDB2_Fake/trunk/tests/QueryTest.php
@@ -10,7 +10,7 @@ require_once "PHPUnit2/Framework/TestSuite.php";
 // You may remove the following line when all tests have been implemented.
 require_once "PHPUnit2/Framework/IncompleteTestError.php";
 
-require_once "../src/libs/Query.php";
+require_once "../MDB/libs/Query.php";
 
 /**
  * Test class for Query.
