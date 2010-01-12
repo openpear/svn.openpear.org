b3c9e8866db0ff9ba538a904fde13ead42b444df
Takeshi Kawamoto <yuki@transrain.net>
2010-01-12 08:28:46 +0000

    トークンの保存、読み込みロジックを外部関数で定義するように変更。 （複数ユーザ対応アプリケーション向け)

--- Services_Twitter/trunk/Services/Twitter/Config.php
+++ Services_Twitter/trunk/Services/Twitter/Config.php
@@ -1,12 +1,30 @@
 <?php
 class Services_Twitter_Config
 {
+
+	/**
+	 * アプリケーション認識キーを保持する
+	 * @var string アプリケーション認識キー
+	 */
+	private $_appKey;
+
+	/**
+	 * 認証認識キーを保存するCookieの名称を保持する
+	 * @var string 認証認識キーを保存するCookieの名称
+	 */
+	private $_cookieName;
+
+	/**
+	 * 接続用トークンを保持する
+	 * @var string 接続用トークン
+	 */
+	private $_token;
+
     /**
-     * 認証情報保存ファイルのパスを保持する
-     *
-     * @var string ファイルパス
+     * 接続用秘密トークンを保持する
+     * @var string 接続用秘密トークン
      */
-    private $_authFile;
+	private $_token_secret;
 
     /**
      * TwitterのOAuth認証に利用されるコンシューマキーを保持する
@@ -33,10 +51,23 @@ class Services_Twitter_Config
     private $_callback;
 
     /**
+     * トークンを読み込む関数を保持する
+     * @var callback トークンを読み込む関数
+     */
+    private $_tokenReadFunc;
+
+    /**
+     * トークンを保存する関数を保持する
+     * @var callback トークンを保存する関数
+     */
+    private $_tokenSaveFunc;
+
+    /**
      * Services_Twitter_Configのインスタンスを作成する
      *
      */
     public function __construct() {
+    	$this->_cookieName = 'stid';
         $this->_authFile = realpath('./') . DIRECTORY_SEPARATOR . 'twitter.auth';
         $this->_consumer_key    = 'Rzt2HVOtG1TmcgtE8r1rQ';
         $this->_consumer_secret = 'DDHJGoElzijet5AWsNWkazAQvhVDaoxSqru20oDrdM';
@@ -55,19 +86,61 @@ class Services_Twitter_Config
     }
 
     /**
-     * 認証情報保存ファイルのパスを設定する
-     * @param string $path 認証情報保存ファイルのパス
+     * アプリケーション認識キーを設定する
+     * @param string $key アプリケーション認識キー
      */
-    public function setAuthFile($path) {
-        $this->_authFile = $path;
+    public function setApplicationKey($key) {
+        $this->_appKey = $key;
     }
 
     /**
-     * 認証情報保存ファイルのパスを取得する
-     * @return string 認証情報保存ファイルのパス
+     * アプリケーション認識キーを取得する
+     * @return string アプリケーション認識キー
      */
-    public function getAuthFile() {
-    	return $this->_authFile;
+    public function getApplicationKey() {
+    	return $this->_appKey;
+    }
+
+    /**
+     * 認証認識キーを保存するCookieの名称を設定する
+     * @param string $name 認証認識キーを保存するCookieの名称
+     */
+    public function setCookieName($name) {
+    	$this->_cookieName = $name;
+    }
+
+    /**
+     * 認証認識キーを保存するCookieの名称を取得する
+     * @return string 認証認識キーを保存するCookieの名称
+     */
+    public function getCookieName() {
+    	return $this->_cookieName;
+    }
+
+    /**
+     * 接続用トークンを設定する
+     * @param string $token トークン
+     * @param string $tokenSecret 秘密トークン
+     */
+    public function setToken($token, $tokenSecret) {
+    	$this->_token = $token;
+    	$this->_token_secret = $tokenSecret;
+    }
+
+    /**
+     * 接続用トークンを取得する
+     * @return string トークン
+     */
+    public function getToken() {
+    	return $this->_token;
+    }
+
+    /**
+     * 接続用秘密トークンを取得する
+     * @return string 秘密トークン
+     */
+    public function getTokenSecret() {
+    	return $this->_token_secret;
     }
 
     /**
@@ -127,4 +200,39 @@ class Services_Twitter_Config
     public function getCallback() {
     	return $this->_callback;
     }
+
+    public function setTokenReadFunction($callback) {
+    	$this->_tokenReadFunc = $callback;
+    }
+
+    public function setTokenSaveFunction($callback) {
+    	$this->_tokenSaveFunc = $callback;
+    }
+
+    public function getAuthInfo() {
+        $authInfo = array(
+             'oauth_access_token' => $this->getToken()
+            ,'oauth_access_token_secret' => $this->getTokenSecret()
+        );
+
+        return $authInfo;
+    }
+
+    public function raiseTokenRead() {
+        $tokens = call_user_func($this->_tokenReadFunc);
+        $this->setToken($tokens['oauth_access_token'], $tokens['oauth_access_token_secret']);
+        return $this->getAuthInfo();
+    }
+
+    public function raiseTokenSave($tokens) {
+        call_user_func($this->_tokenSaveFunc, $tokens);
+    }
+
+
+/*
+    private $_tokenReadFunc;
+    private $_tokenSaveFunc;
+$config->setAuthInfoReaderFunction('readTokens');
+$config->setAuthInfoRegisterFunction('saveTokens');
+ */
 }
--- Services_Twitter/trunk/example.php
+++ Services_Twitter/trunk/example.php
@@ -5,17 +5,23 @@ require_once('Services/Twitter.php');
 // Services_Twitterの設定情報
 $config = new Services_Twitter_Config();
 
+// アプリケーション認識キーの設定
+$config->setApplicationKey(sha1('Services_Twitter Example Code'));
+
+// 登録情報のCookieキーの設定
+$config->setCookieName('services_twitter_id');
+
 // コンシューマキーの設定
 // テスト値：Services_Twitterのコンシューマキーを利用
 $config->setConsumer('Rzt2HVOtG1TmcgtE8r1rQ','DDHJGoElzijet5AWsNWkazAQvhVDaoxSqru20oDrdM');
 
-// 認証情報ファイルのパス設定
-$config->setAuthFile(dirname(__FILE__) . DIRECTORY_SEPARATOR . '.twitter');
+// トークンの読み込み・書き込み処理を登録
+$config->setTokenReadFunction('readTokens');
+$config->setTokenSaveFunction('saveTokens');
 
 // 認証ページ、認証コールバックURIの設定
 $config->setAuthPage(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'example.template.php');
 $config->setCallback('http://localhost/stw/trunk/example.php');
-
 
 // Services_Twitterのインスタンス化
 $tweet = new Services_Twitter($config);
@@ -24,10 +30,32 @@ $tweet = new Services_Twitter($config);
 //未認証時は認証用のページが表示される。
 $tweet->connect();
 
-// APIの利用状況を取得する。
-$limit = $tweet->getRateLimitStatus();
-debug($limit);
+// 認証情報の設定
+if ($tweet->isAuthorized()) {
+    // APIの利用状況を取得する。
+    $limit = $tweet->getRateLimitStatus();
+    debug($limit);
+}
 
+/**
+ * 保存したトークンを取得する
+ */
+function readTokens() {
+    $authfile = dirname(__FILE__) . DIRECTORY_SEPARATOR . '.twitter';
+    if (file_exists($authfile)) {
+        return unserialize(file_get_contents($authfile));
+    }
+
+    return null;
+}
+
+/**
+ * トークンを保存する
+ */
+function saveTokens($tokens) {
+    $authfile = dirname(__FILE__) . DIRECTORY_SEPARATOR . '.twitter';
+    file_put_contents($authfile, serialize($tokens));
+}
 
 function debug($value) {
 	echo '<pre>';
