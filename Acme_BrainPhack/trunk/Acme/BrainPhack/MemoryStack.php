71069de1950c3cb2e6b5d0a18c2deda813ecc3cb
msakamoto-sf <msakamoto-sf@224e63c0-5457-0410-989b-907d9cf58824>
2010-01-17 07:43:09 +0000

    - add BrainF*ck memory stack model, and test cases.

---
+++ Acme_BrainPhack/trunk/Acme/BrainPhack/MemoryStack.php
@@ -0,0 +1,189 @@
+<?php
+/*
+ *   Copyright (c) 2007 msakamoto-sf <msakamoto-sf@users.sourceforge.net>
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.*
+ */
+
+/**
+ * @package Acme_BrainPhack
+ * @author msakamoto-sf <msakamoto-sf@users.sourceforge.net>
+ * @version $Id$
+ */
+
+/**
+ * BrainF*ck MemoryStack
+ *
+ * @author msakamoto-sf <sakamoto-gsyc-3s@glamenv-septzen.net>
+ * @package Acme_BrainPhack
+ * @since 0.0.1
+ */
+class Acme_BrainPhack_MemoryStack
+{
+    // {{{ properties
+
+    /**
+     * maximum stack size
+     *
+     * @var integer
+     * @access protected
+     */
+    var $_stack_size = 0;
+
+    /**
+     * memory stack
+     *
+     * @var array
+     * @access protected
+     */
+    var $_stack;
+
+    /**
+     * memory pointer address
+     *
+     * @var integer
+     * @access protected
+     */
+    var $_curr_ptr = 0;
+
+    // }}}
+    // {{{ constructor
+
+    /**
+     * Constructor
+     *
+     * @access public
+     * @param integer maximum stack size
+     * @param mixed initial memory value (optional)
+     *              If omitted, memory stack is initialized by 0(zero).
+     */
+    function Acme_BrainPhack_MemoryStack($sz, $init = 0)
+    {
+        $this->_stack = array();
+        $this->_stack_size = $sz;
+        for ($i = 0; $i < $sz; $i++) {
+            $this->_stack[$i] = $init;
+        }
+    }
+
+    // }}}
+    // {{{ memory value getter/setter
+
+    /**
+     * return memory value
+     *
+     * @access public
+     * @param integer memory address(optional)
+     *                If omitted, current pointer address is used.
+     * @return mixed pointed memory value.
+     *  If memory address < 0 or maximum stack size < memory address, 
+     *  return false.
+     */
+    function get($i = null)
+    {
+        if (is_null($i)) {
+            return $this->_stack[$this->_curr_ptr];
+        }
+        if (0 > $i) {
+            return false;
+        }
+        if ($i >= $this->_stack_size) {
+            return false;
+        }
+        return $this->_stack[$i];
+    }
+
+    /**
+     * set new memory value
+     *
+     * @access public
+     * @param mixed new value
+     * @param integer memory address(optional)
+     *                If omitted, current pointer address is used.
+     * @return mixed pointed old memory value.
+     *  If memory address < 0 or maximum stack size < memory address, 
+     *  return false and nothing changes.
+     */
+    function set($v, $i = null)
+    {
+        if (is_null($i)) {
+            $old = $this->_stack[$this->_curr_ptr];
+            $this->_stack[$this->_curr_ptr] = $v;
+            return $old;
+        }
+        if (0 > $i) {
+            return false;
+        }
+        if ($i >= $this->_stack_size) {
+            return false;
+        }
+        $old = $this->_stack[$i];
+        $this->_stack[$i] = $v;
+        return $old;
+    }
+
+    // }}}
+    // {{{ pointer address increment/decrement
+
+    /**
+     * increment pointer address
+     *
+     * @access public
+     * @return array array('old pointer address', 'new pointer address')
+     *  If incremented memory address WILL BE lesser than < 0 or 
+     *  oversized maximum stack size, memory address doesn't be incremented
+     *  (nothing changed), and return false
+     */
+    function ptr_inc()
+    {
+        $old = $this->_curr_ptr;
+        if ($this->_curr_ptr == $this->_stack_size) {
+            return false;
+        }
+        $this->_curr_ptr++;
+        return array($old, $this->_curr_ptr);
+    }
+
+    /**
+     * decrement pointer address
+     *
+     * @access public
+     * @return array array('old pointer address', 'new pointer address')
+     *  If decremented memory address WILL BE lesser than < 0 or 
+     *  oversized maximum stack size, memory address doesn't be incremented
+     *  (nothing changed), and return false
+     */
+    function ptr_dec()
+    {
+        $old = $this->_curr_ptr;
+        if (0 == $this->_curr_ptr) {
+            return false;
+        }
+        $this->_curr_ptr--;
+        return array($old, $this->_curr_ptr);
+    }
+
+    // }}}
+}
+
+/**
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ * vim: set expandtab tabstop=4 shiftwidth=4:
+ */
---
+++ Acme_BrainPhack/trunk/test/MemoryStack_TestCase.php
@@ -0,0 +1,161 @@
+<?php
+/*
+ *   Copyright (c) 2007 msakamoto-sf <msakamoto-sf@users.sourceforge.net>
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.*
+ */
+
+/**
+ * @package Acme_BrainPhack
+ * @author msakamoto-sf <msakamoto-sf@users.sourceforge.net>
+ * @version $Id$
+ */
+
+/**
+ * requires
+ */
+require_once('Acme/BrainPhack/MemoryStack.php');
+
+class MemoryStack_TestCase extends UnitTestCase
+{
+
+    function testConstructorAndPointerIncDec()
+    {
+        // create 3 sized memory stack
+        $bpms =& new Acme_BrainPhack_MemoryStack(3);
+
+        // decrement -> lesser than 0, return false
+        $r = $bpms->ptr_dec();
+        $this->assertIdentical(false, $r);
+
+        list($old, $new) = $bpms->ptr_inc();
+        $this->assertIdentical(0, $old);
+        $this->assertIdentical(1, $new);
+
+        list($old, $new) = $bpms->ptr_inc();
+        $this->assertIdentical(1, $old);
+        $this->assertIdentical(2, $new);
+
+        list($old, $new) = $bpms->ptr_inc();
+        $this->assertIdentical(2, $old);
+        $this->assertIdentical(3, $new);
+
+        // increment -> size over, return false
+        $r = $bpms->ptr_inc();
+        $this->assertIdentical(false, $r);
+
+        list($old, $new) = $bpms->ptr_dec();
+        $this->assertIdentical(3, $old);
+        $this->assertIdentical(2, $new);
+
+        list($old, $new) = $bpms->ptr_dec();
+        $this->assertIdentical(2, $old);
+        $this->assertIdentical(1, $new);
+
+        list($old, $new) = $bpms->ptr_dec();
+        $this->assertIdentical(1, $old);
+        $this->assertIdentical(0, $new);
+    }
+
+    function testGetterSetterAndMemoryInitialize()
+    {
+        // default, initialized by 0
+        $bpms =& new Acme_BrainPhack_MemoryStack(3);
+        $r = $bpms->get();
+        $this->assertIdentical(0, $r);
+        $bpms->ptr_inc();
+        $r = $bpms->get();
+        $this->assertIdentical(0, $r);
+        $bpms->ptr_inc();
+        $r = $bpms->get();
+        $this->assertIdentical(0, $r);
+
+        // initialized by 1
+        $bpms =& new Acme_BrainPhack_MemoryStack(3, 1);
+        $r = $bpms->get();
+        $this->assertIdentical(1, $r);
+        $bpms->ptr_inc();
+        $r = $bpms->get();
+        $this->assertIdentical(1, $r);
+        $bpms->ptr_inc();
+        $r = $bpms->get();
+        $this->assertIdentical(1, $r);
+
+        // initialized by 'A'
+        $bpms =& new Acme_BrainPhack_MemoryStack(3, 'A');
+        $r = $bpms->get();
+        $this->assertIdentical('A', $r);
+        $bpms->ptr_inc();
+        $r = $bpms->get();
+        $this->assertIdentical('A', $r);
+        $bpms->ptr_inc();
+        $r = $bpms->get();
+        $this->assertIdentical('A', $r);
+
+        // setter test
+        $old = $bpms->set('B');
+        $this->assertIdentical('A', $r);
+        $r = $bpms->get();
+        $this->assertIdentical('B', $r);
+
+        // get/set with pointer address
+        $r = $bpms->get(1);
+        $this->assertIdentical('A', $r);
+        $old = $bpms->set('C', 1);
+        $this->assertIdentical('A', $r);
+        $r = $bpms->get(1);
+        $this->assertIdentical('C', $r);
+        $r = $bpms->get(2);
+        $this->assertIdentical('B', $r);
+    }
+
+    function testGetterSetterNearBorder()
+    {
+        // size = 3, address range = 0 - 2.
+        $bpms =& new Acme_BrainPhack_MemoryStack(3);
+
+        // < 0
+        $r = $bpms->get(-1);
+        $this->assertIdentical(false, $r);
+        $r = $bpms->get(-2);
+        $this->assertIdentical(false, $r);
+        $r = $bpms->set(100, -1);
+        $this->assertIdentical(false, $r);
+        $r = $bpms->set(100, -2);
+        $this->assertIdentical(false, $r);
+
+        // > 3
+        $r = $bpms->get(3);
+        $this->assertIdentical(false, $r);
+        $r = $bpms->get(4);
+        $this->assertIdentical(false, $r);
+        $r = $bpms->set(100, 3);
+        $this->assertIdentical(false, $r);
+        $r = $bpms->set(100, 4);
+        $this->assertIdentical(false, $r);
+
+
+    }
+}
+
+/**
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ * vim: set expandtab tabstop=4 shiftwidth=4:
+ */
