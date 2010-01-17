bdcba7b3a5897ad803afa19f710a391276623529
anatoo <anatoo.jp@gmail.com>
2010-01-17 07:02:00 +0000

    added PEG_ErrorReporter and PEG::error() and so on

---
+++ PEG/trunk/test/ErrorReporter.php
@@ -0,0 +1,14 @@
+<?php
+include_once dirname(__FILE__) . '/t/t.php';
+
+$lime = new lime_test;
+
+$p = PEG::error('hoge');
+$c = PEG::context('fuga');
+
+$p->parse($c);
+$lime->is($c->lastError(), array(0, 'hoge'));
+
+$c = PEG::context(array(1, 2));
+$p->parse($c);
+$lime->is($c->lastError(), array(0, 'hoge'));
