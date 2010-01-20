de127547a35968058f942963532ccca59dfeb6e7
anatoo <anatoo.jp@gmail.com>
2010-01-20 13:44:47 +0000

    for commit failure

--- HatenaSyntax/branches/fast/test/t.php
+++
@@ -1,3 +0,0 @@
-<?php
-include_once dirname(__FILE__) . '/lime.php';
-include_once dirname(__FILE__) . '/../lib/HatenaSyntax.php';
--- HatenaSyntax/branches/fast/test/testall.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once dirname(__FILE__) . '/t.php';
-
-$lime = new lime_harness(null);
-$lime->register_glob(dirname(__FILE__) . '/lib/*.php');
-$lime->register_glob(dirname(__FILE__) . '/lib/HatenaSyntax/*.php');
-$lime->run();
