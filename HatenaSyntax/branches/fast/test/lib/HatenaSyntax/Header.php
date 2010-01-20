19e7568e44584cb19786497247d1574861097b6b
anatoo <anatoo.jp@gmail.com>
2010-01-20 13:39:37 +0000

    for commit failure

--- HatenaSyntax/branches/fast/test/lib/HatenaSyntax/Header.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once dirname(__FILE__) . '/../../t.php';
-
-$t = new lime_test;
-
-$p = new HatenaSyntax_Header(PEG::anything());
-$c = PEG::context(array('*abc'));
-
-$t->is($p->parse($c), array(0, array('a', 'b', 'c')));
