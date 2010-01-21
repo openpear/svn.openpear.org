c6e38262289c696efd67ceae0147d74dc945face
FreeBSE <FreeBSE@224e63c0-5457-0410-989b-907d9cf58824>
2010-01-21 15:01:35 +0000

    バージョンの修正 既存の天気情報のデータ取得メソッドを生データを返すメソッドに修正 フォーマットされた天気情報を返すメソッドを新たに追加

--- Services_WeatherUnderground/trunk/WeatherUnderground.php
+++ Services_WeatherUnderground/trunk/WeatherUnderground.php
@@ -1,10 +1,10 @@
 <?php
 /**
- *  Services_WeatherUnderground 0.0.1
+ *  Services_WeatherUnderground 0.1.0
  *
  *  @author	    FreeBSE <freebse@live.jp> <http://panasocli.cc/wordpress>
  *  @package	Services_WeatherUnderground
- *  @version	Services_WeatherUnderground v 0.0.1 2009/01/06
+ *  @version	Services_WeatherUnderground v 0.1.0 2009/01/21
  *
  */
 
@@ -16,6 +16,7 @@ define('CITY_NOT_FOUND', 0x01);
 
 interface WeatherUnderground{
     public function getWeatherData();
+    public function getRawWeatherData();
 }
 
 class Services_WeatherUnderground implements WeatherUnderground {
@@ -70,14 +71,99 @@ class Services_WeatherUnderground implements WeatherUnderground {
 	}
 
 	/**
-	 * 天気情報を取得する
+	 * WUGの生の天気情報を取得する
+	 * @return Array
+	 */
+	public function getRawWeatherData(){
+	    if(!$this->weather['station_id']){
+		return CITY_NOT_FOUND;
+	    }
+	    return $this->weather;
+	}
+
+	/**
+	 * わかりやすい形で天気情報を取得する
 	 * @return Array
 	 */
 	public function getWeatherData(){
 	    if(!$this->weather['station_id']){
 		return CITY_NOT_FOUND;
 	    }
-	    return $this->weather;
+	    switch($this->weather['wind_dir']){
+		case 'NNW':
+		    $win_dir = '北北西';
+		    break;
+		case 'NW':
+		    $win_dir = '北西';
+		    break;
+		case 'WNW':
+		    $win_dir = '西北西';
+		    break;
+		case 'N':
+		    $win_dir = '北';
+		    break;
+		case 'E':
+		    $win_dir = '東';
+		    break;
+		case 'NE':
+		    $win_dir = '北東';
+		    break;
+		case 'NNE':
+		    $win_dir = '北北東';
+		    break;
+		case 'ENE':
+		    $win_dir = '東北東';
+		    break;
+		case 'S':
+		    $win_dir = '南';
+		    break;
+		case 'SE':
+		    $win_dir = '南東';
+		    break;
+		case 'SSE':
+		    $win_dir = '南南東';
+		    break;
+		case 'ESE':
+		    $win_dir = '東南東';
+		    break;
+		case 'WSW':
+		    $win_dir = '西南西';
+		    break;
+		case 'SSW':
+		    $win_dir = '南南西';
+		    break;
+		case 'SW':
+		    $win_dir = '南西';
+		    break;
+	    }
+
+	    //風速変換
+	    $mph = $this->weather['wind_mph'] / 2 *1000 / 60 / 60;
+	    $mph = (int) $mph;
+
+	    $weather = array(
+		//街
+		'city' => $this->weather['display_location']['city'],
+		//国
+		'country' => $this->weather['display_location']['state_name'],
+		//天気画像
+		'image' => $this->weather['icon_url_base'] . $this->weather['icon'] . $this->weather['icon_url_name'],
+		//観測地
+		'observation_location' => $this->weather['observation_location']['city'],
+		//天気
+		//'weather' => $this->weather['weather'],
+		//気温
+		'temp' => $this->weather['temp_c'] . '℃',
+		//湿度
+		'humidity' => $this->weather['relative_humidity'],
+		//風向
+		'wind_dir' => $win_dir,
+		//風速
+		'wind_speed' => $mph . 'm/s',
+		//気圧
+		'pressure' => $this->weather['pressure_mb'] . ' hPa',
+		    );
+	    return $weather;
 	}
 }
 ?>
