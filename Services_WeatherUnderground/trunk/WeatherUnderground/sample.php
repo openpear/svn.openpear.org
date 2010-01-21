28109f85c8692541fa670b53e31e0cd6aaf865fb
FreeBSE <freebse@panasocli.cc>
2010-01-21 15:01:35 +0000

    バージョンの修正 既存の天気情報のデータ取得メソッドを生データを返すメソッドに修正 フォーマットされた天気情報を返すメソッドを新たに追加

--- Services_WeatherUnderground/trunk/WeatherUnderground/sample.php
+++ Services_WeatherUnderground/trunk/WeatherUnderground/sample.php
@@ -11,6 +11,91 @@ if($wg->getWeatherData() === CITY_NOT_FOUND){
     print_r($wg->getWeatherData());
 }
 
+$weather = $wg->getWeatherData();
+?>
+<table border="1">
+    <tr>
+	<td>
+	    国
+	</td>
+	<td>
+	    <?php echo $weather['country'] ?>
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    街
+	</td>
+	<td>
+	    <?php echo $weather['city'] ?>
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    観測地
+	</td>
+	<td>
+	    <?php echo $weather['observation_location'] ?>
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    天気
+	</td>
+	<td>
+	    <img src="<?php echo $weather['image'] ?>">
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    気温
+	</td>
+	<td>
+	    <?php echo $weather['temp'] ?>
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    湿度
+	</td>
+	<td>
+	    <?php echo $weather['humidity'] ?>
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    風向
+	</td>
+	<td>
+	    <?php echo $weather['wind_dir'] ?>
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    風速
+	</td>
+	<td>
+	    <?php echo $weather['wind_speed'] ?>
+	</td>
+    </tr>
+    <tr>
+	<td>
+	    気圧
+	</td>
+	<td>
+	    <?php echo $weather['pressure'] ?>
+	</td>
+    </tr>
+</table>
+<?php
+
+$wg = new Services_WeatherUnderground('Test');
+if($wg->getWeatherData() === CITY_NOT_FOUND){
+    echo sprintf('指定された観測地は見つかりませんでした「Error Code 0x%02X」', $wg->getWeatherData());
+}else{
+    print_r($wg->getWeatherData());
+}
+
 /*
 foreach($wg->weather as $key => $val){
     echo $key , " = " , $val."<br>";
