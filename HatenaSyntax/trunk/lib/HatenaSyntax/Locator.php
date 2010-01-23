b80522883bbe8f393e61a0e5a127984131f625a9
anatoo <anatoo.jp@gmail.com>
2010-01-23 10:05:46 +0000

    replaced to branches/fast

---
+++ HatenaSyntax/trunk/lib/HatenaSyntax/Locator.php
@@ -0,0 +1,213 @@
+<?php
+/**
+ * @package HatenaSyntax
+ * @author anatoo<anatoo@nequal.jp>
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ * @version $Id$
+ */
+
+class HatenaSyntax_Locator
+{
+    protected $block_ref = null;
+    protected $shared = array();
+    protected $facade;
+    
+    private function __construct()
+    {
+        $this->setup();
+    }
+
+    static function it()
+    {
+        static $obj = null;
+        return $obj ? $obj : $obj = new self;
+    }
+    
+    function __get($name)
+    {
+        return isset($this->shared[$name]) ? 
+            $this->shared[$name] : 
+            $this->shared[$name] = $this->{'create' . $name}();
+    }
+
+    protected function nodeCreater($type, PEG_IParser $parser, Array $keys = array())
+    {
+        return new HatenaSyntax_NodeCreater($type, $parser, $keys);
+    }
+    
+    protected function createLineChar()
+    {
+        return PEG::anything();
+    }
+    
+    protected function createFootnote()
+    {
+        $elt = PEG::subtract(
+            PEG::choice($this->bracket, $this->lineChar), 
+            '))');
+                            
+        $parser = PEG::pack('((', 
+                            PEG::many1($elt), 
+                            '))');
+                            
+        return $this->nodeCreater('footnote', $parser);
+    }
+    
+    protected function createLineElement()
+    {
+        return new HatenaSyntax_LineElement($this->bracket, $this->footnote);
+    }
+    
+    protected function createLineSegment()
+    {
+        return PEG::many($this->lineElement);
+    }
+    
+    protected function createHttpLink()
+    {
+        $title_char = PEG::subtract($this->lineChar, ']');
+        $title = PEG::second(':title=', PEG::join(PEG::many1($title_char)));
+        
+        $url_char = PEG::subtract($this->lineChar, ']', ':title=');
+        $url = PEG::join(PEG::seq(
+            PEG::choice('http://', 'https://'), 
+            PEG::many1($url_char))); 
+
+        $parser = PEG::seq($url, PEG::optional($title));
+        
+        return $this->nodeCreater('httplink', $parser, array('href', 'title'));
+    }
+    
+    protected function createImageLink()
+    {
+        $url_char = PEG::subtract($this->lineChar, ']', ':image]');
+        $url = PEG::join(PEG::seq(
+            PEG::choice('http://', 'https://'), 
+            PEG::many1($url_char)));
+                                  
+        $parser = PEG::first($url, ':image');
+        
+        return $this->nodeCreater('imagelink', $parser);
+    }
+    
+    protected function createKeywordLink()
+    {
+        $body = PEG::join(PEG::many1(PEG::subtract($this->lineChar, ']]')));
+        $body = PEG::subtract($body, 'javascript:');
+        $parser = PEG::pack('[', $body, ']');
+        
+        return $this->nodeCreater('keywordlink', $parser);
+    }
+    
+    protected function createNullLink()
+    {
+        $body = PEG::join(PEG::many1(PEG::subtract($this->lineChar, '[]')));
+        $parser = PEG::pack(']', $body, '[');
+        
+        return $parser;
+    }
+    
+    protected function createTableOfContents()
+    {
+        $parser = new HatenaSyntax_Regex('/^\[:contents]$/');
+        
+        return $this->nodeCreater('tableofcontents', $parser);
+    }
+    
+    protected function createInlineTableOfContents()
+    {
+        $parser = PEG::token(':contents');
+        return $this->nodeCreater('tableofcontents', $parser);
+    }
+    
+    protected function createBracket()
+    {
+        return PEG::pack('[', PEG::choice(
+            $this->inlineTableOfContents, 
+            $this->nullLink, 
+            $this->keywordLink, 
+            $this->imageLink, 
+            $this->httpLink), 
+        ']');
+    }
+
+    protected function createDefinitionList()
+    {
+        $parser = new HatenaSyntax_DefinitionList($this->lineElement);
+        return $this->nodeCreater('definitionlist', $parser);
+    }
+    
+    protected function createPre()
+    {
+        $parser = new HatenaSyntax_Pre($this->lineElement);
+        
+        return $this->nodeCreater('pre', $parser);
+    }
+    
+    protected function createSuperPre()
+    {
+        $parser = new HatenaSyntax_SuperPre;
+
+        return $this->nodeCreater('superpre', $parser, array('type', 'body'));
+    }
+    
+    protected function createHeader()
+    {
+        $parser = new HatenaSyntax_Header($this->lineElement);
+        
+        return $this->nodeCreater('header', $parser, array('level', 'body'));
+    }
+
+    protected function createList()
+    {
+        $parser = new HatenaSyntax_List($this->lineElement);
+        
+        return $this->nodeCreater('list', $parser);
+    }
+    
+    protected function createTable()
+    {
+        $parser = new HatenaSyntax_Table($this->lineElement);
+        
+        return $this->nodeCreater('table', $parser);
+    }
+
+    protected function createBlockQuote()
+    {
+        $parser = new HatenaSyntax_Quote($this->block);
+                                      
+        return $this->nodeCreater('blockquote', $parser, array('url', 'body'));
+    }
+    
+    protected function createParagraph()
+    {
+        $parser = new HatenaSyntax_Paragraph($this->lineElement);
+        
+        return $this->nodeCreater('paragraph', $parser);
+    }
+    
+    protected function createEmptyParagraph()
+    {
+        $parser = PEG::count(PEG::many1(PEG::token('')));
+
+        return $this->nodeCreater('emptyparagraph', $parser);
+    }
+    
+    protected function createBlock()
+    {
+        $parser = PEG::ref($r);
+        $this->block_ref = &$r;
+        return $parser;
+    }
+
+    protected function createParser()
+    {
+        return $this->nodeCreater('root', PEG::many($this->block));
+    }
+    
+    protected function setup()
+    {
+        $this->block;
+        $this->block_ref = PEG::memo(new HatenaSyntax_Block($this));
+    }
+}
--- HatenaSyntax/trunk/test/t/testall.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once dirname(__FILE__) . '/t.php';
-
-$lime = new lime_harness(null);
-$lime->register_glob(dirname(__FILE__) . '/../*.php');
-$lime->run();
