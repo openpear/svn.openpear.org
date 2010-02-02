19b89b3b86c341fc21a80f192d1702cbb9518893
anatoo <anatoo.jp@gmail.com>
2010-02-02 08:01:40 +0000

    added PEG::hook() and PEG::parserOf() and PEG_InstantParser

--- PEG/trunk/test/t/testall.php
+++ PEG/trunk/test/t/testall.php
@@ -2,5 +2,5 @@
 include_once dirname(__FILE__) . '/t.php';
 
 $h = new lime_harness(new lime_output);
-$h->register_glob('../*.php');
+$h->register_glob(dirname(__FILE__) . '/../*.php');
 $h->run();
