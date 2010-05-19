b166d2e1a616ba45ff749de4f62302168ba5d432
OGAWA Katsuhiro <fivestar@users.noreply.github.com>
2010-05-19 05:05:23 +0000

    propel sucks.

--- Wozozo_PropelUtil/trunk/src/Wozozo/PropelUtil.php
+++ Wozozo_PropelUtil/trunk/src/Wozozo/PropelUtil.php
@@ -7,16 +7,34 @@
  */
 class Wozozo_PropelUtil
 {
-    public static function toArray($obj, $keyType = BasePeer::TYPE_FIELDNAME)
+    public static function toArray($obj, $keyType = BasePeer::TYPE_FIELDNAME, &$models = array())
     {
         $array = array();
 
         if (is_array($obj)) {
             foreach ($obj as $k => $v) {
-                $array[$k] = self::toArray($v, $keyType);
+                $array[$k] = self::toArray($v, $keyType, $models);
             }
         } else if ($obj instanceof BaseObject) {
+            $models[strtolower(get_class($obj))] = strtolower(get_class($obj));
+
             $array = $obj->toArray($keyType);
+
+            $relations = $obj->getPeer()->getTableMap()->getRelations();
+            foreach ($relations as $name => $relation) {
+                if (in_array(strtolower($name), $models)) {
+                    continue;
+                }
+
+                $method = 'get'.$name;
+                if (in_array($relation->getType(), array(RelationMap::ONE_TO_MANY))) {
+                    $method .= 's';
+                }
+
+                if (method_exists($obj, $method)) {
+                    $array[$name] = self::toArray($obj->$method(), $keyType, $models);
+                }
+            }
         }
 
         return $array;
