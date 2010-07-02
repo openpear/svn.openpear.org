f01050d39037f78d3d6ffc06edeb62f1783c2a4d
sasezaki <sasezaki@224e63c0-5457-0410-989b-907d9cf58824>
2010-07-02 12:40:28 +0000

    add Exceptions

--- Diggin_Http_Response_Charset/trunk/library/Diggin/Http/Response/Charset/Front/ConvertInterface.php
+++ Diggin_Http_Response_Charset/trunk/library/Diggin/Http/Response/Charset/Front/ConvertInterface.php
@@ -20,8 +20,8 @@
  * ---------------------------------------------------------------------
  */
 
-interface Diggin_Http_Response_Charset_Front_EncodeInterface
+interface Diggin_Http_Response_Charset_Front_ConvertInterface
 {
-    public function encode($content, $remains = null);
+    public function convert($content, $remains = null);
 }
 
