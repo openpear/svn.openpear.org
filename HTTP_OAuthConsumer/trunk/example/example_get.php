3d2c0b1594036973feaac03d1d2da22f5ba3ffb2
Yoshida Tetsuya <yoshida.eth0@gmail.com>
2010-08-03 20:29:40 +0000

    import

---
+++ HTTP_OAuthConsumer/trunk/example/example_get.php
@@ -0,0 +1,15 @@
+<?php
+require_once('HTTP/OAuthConsumer.php');
+
+try {
+	$oauth = HTTP_OAuthConsumer::factory();
+	$oauth->setURL('http://example.com/?format=json');
+	$oauth->addPostParameter('aaa', 'AAA');
+	$oauth->setConsumer('testuser', 'testpass');
+	$res = $oauth->send();
+	print_r($res);
+} catch(Exception $e) {
+	echo $e->getMessage();
+}
+echo "\n";
+
