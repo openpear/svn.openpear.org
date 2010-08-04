d0e1d1fb7c91e8b165eb8768ef229b67f960f598
eth0jp <eth0jp@224e63c0-5457-0410-989b-907d9cf58824>
2010-08-04 19:51:46 +0000

    mod file_exists=>is_file

--- HTTP_OAuthConsumer/trunk/src/HTTP/OAuthConsumer/RSA_SHA1.php
+++ HTTP_OAuthConsumer/trunk/src/HTTP/OAuthConsumer/RSA_SHA1.php
@@ -6,7 +6,7 @@ class HTTP_OAuthConsumer_RSA_SHA1 extends HTTP_OAuthConsumer
 	public function setConsumer($consumer_key, $privatekey, $is_file=false)
 	{
 		if ($is_file) {
-			if (!file_exists($privatekey)) {
+			if (!is_file($privatekey)) {
 				throw new HTTP_OAuthConsumer_Exception('No such private key file');
 			}
 			$privatekey = file_get_contents($privatekey);
