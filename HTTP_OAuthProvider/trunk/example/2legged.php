da7e932cfe7ea6971840cca283afee510b730a13
eth0jp <eth0jp@224e63c0-5457-0410-989b-907d9cf58824>
2010-08-09 06:20:04 +0000

    mod example

--- HTTP_OAuthProvider/trunk/example/2legged.php
+++ HTTP_OAuthProvider/trunk/example/2legged.php
@@ -6,9 +6,10 @@ $o = new HTTP_OAuthProvider();
 try {
 	$o->setGetConsumerHandler('getConsumer');
 	$o->authenticate();
-	echo "Auth OK!!!";
+	echo "Auth OK!!!\n";
+	printf("consumer: %s\n", $o->getConsumer()->getKey());
+
 } catch(Exception $e) {
 	header(sprintf('HTTP/1.0 %d', $e->getCode()));
 	echo $e->getMessage();
 }
-echo "\n";
