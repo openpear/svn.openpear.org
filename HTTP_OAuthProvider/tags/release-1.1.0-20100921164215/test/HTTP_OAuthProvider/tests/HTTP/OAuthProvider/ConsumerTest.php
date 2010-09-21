9d17af773f0fa029ef9ac0f6273f3f0ec8786aba
openpear <openpear@224e63c0-5457-0410-989b-907d9cf58824>
2010-09-21 07:42:15 +0000

    [Package Release] HTTP_OAuthProvider

---
+++ HTTP_OAuthProvider/tags/release-1.1.0-20100921164215/test/HTTP_OAuthProvider/tests/HTTP/OAuthProvider/ConsumerTest.php
@@ -0,0 +1,96 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * OAuth authentication class for service provider.
+ *
+ * PHP versions 5
+ *
+ * @category  HTTP
+ * @package   OAuthProvider
+ * @author    Tetsuya Yoshida <tetu@eth0.jp>
+ * @copyright 2010 Tetsuya Yoshida
+ * @license   http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version   1.1.0
+ * @link      http://openpear.org/package/HTTP_OAuthProvider
+ */
+require_once 'PHPUnit/Framework.php';
+require_once 'HTTP/OAuthProvider.php';
+require_once 'HTTP/OAuthProvider/Mock/HMAC_SHA1.php';
+
+/**
+ * HTTP_OAuthProvider_ConsumerTest
+ *
+ * @category HTTP
+ * @package  OAuthProvider
+ * @author   Tetsuya Yoshida <tetu@eth0.jp>
+ * @license  http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version  1.1.0
+ * @link     http://openpear.org/package/HTTP_OAuthProvider
+ */
+class HTTP_OAuthProvider_ConsumerTest extends PHPUnit_Framework_TestCase
+{
+    /** 
+     * setUp
+     * 
+     * Set up a mock object.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        $provider = new HTTP_OAuthProvider_Mock_HMAC_SHA1();
+        $this->consumer = $provider->getConsumer();
+    }
+
+    /** 
+     * testInstanceType
+     * 
+     * Check a instance type.
+     *
+     * @return void
+     *
+     * @test
+     */
+    public function testInstanceType()
+    {
+        $this->assertType('HTTP_OAuthProvider_Consumer', $this->consumer);
+    }
+
+    /** 
+     * testGetterMethod
+     * 
+     * Check a getter method.
+     *
+     * @return void
+     *
+     * @test
+     */
+    public function testGetterMethod()
+    {
+        $publickey = '-----BEGIN CERTIFICATE-----
+MIIDijCCAvOgAwIBAgIJAOXBQLEpMB4rMA0GCSqGSIb3DQEBBQUAMIGLMQswCQYD
+VQQGEwJKUDEOMAwGA1UECBMFVG9reW8xETAPBgNVBAcTCFNoaW5qdWt1MRAwDgYD
+VQQKEwdleGFtcGxlMRAwDgYDVQQLEwdleGFtcGxlMRQwEgYDVQQDEwtleGFtcGxl
+LmNvbTEfMB0GCSqGSIb3DQEJARYQcm9vdEBleGFtcGxlLmNvbTAeFw0wOTEwMTUw
+ODMyNDdaFw0xOTEwMTMwODMyNDdaMIGLMQswCQYDVQQGEwJKUDEOMAwGA1UECBMF
+VG9reW8xETAPBgNVBAcTCFNoaW5qdWt1MRAwDgYDVQQKEwdleGFtcGxlMRAwDgYD
+VQQLEwdleGFtcGxlMRQwEgYDVQQDEwtleGFtcGxlLmNvbTEfMB0GCSqGSIb3DQEJ
+ARYQcm9vdEBleGFtcGxlLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEA
+orhSQotOymjP+lnDqRvrlYWKzd3M8vE82U7emeS9KQPtCBoy+fXP/kMEMxG/YU+c
+NAS/2BLFGN48EPM0ZAQap384nx+TNZ6sGuCJa60go8yIWff72DZjSZI6otfPjC9S
+NlxOnNLNAfGWAiaCcuBP1uJVhyrs1pu7SaEXBOP4pQ0CAwEAAaOB8zCB8DAdBgNV
+HQ4EFgQU3mEIdWrvKu+yuwIJD2WczQLI3j4wgcAGA1UdIwSBuDCBtYAU3mEIdWrv
+Ku+yuwIJD2WczQLI3j6hgZGkgY4wgYsxCzAJBgNVBAYTAkpQMQ4wDAYDVQQIEwVU
+b2t5bzERMA8GA1UEBxMIU2hpbmp1a3UxEDAOBgNVBAoTB2V4YW1wbGUxEDAOBgNV
+BAsTB2V4YW1wbGUxFDASBgNVBAMTC2V4YW1wbGUuY29tMR8wHQYJKoZIhvcNAQkB
+FhByb290QGV4YW1wbGUuY29tggkA5cFAsSkwHiswDAYDVR0TBAUwAwEB/zANBgkq
+hkiG9w0BAQUFAAOBgQAO2ZKL0/tPhpVfbOoSXl+tlmTyyb8w7mCnjYYWwcwUAf1N
+ylgYxKPrKfamjZKpeRY487VbTee1jfud709oIK5l9ghjz64kPRn/AYHTRwRkBKbb
+wuBWH4L6Rw3ml0ODXW64bdTx/QsAv5M1SyCp/nl8R27dz3MX2D1Ov2o4ipTlZw==
+-----END CERTIFICATE-----';
+        $this->assertEquals('testuser', $this->consumer->getKey());
+        $this->assertEquals('testpass', $this->consumer->getSecret());
+        $this->assertEquals($publickey, $this->consumer->getPublicKey());
+    }
+}
---
+++ HTTP_OAuthProvider/tags/release-1.1.0-20100921164215/test/HTTP_OAuthProvider/tests/HTTP/OAuthProvider/SignatureTest.php
@@ -0,0 +1,78 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * OAuth authentication class for service provider.
+ *
+ * PHP versions 5
+ *
+ * @category  HTTP
+ * @package   OAuthProvider
+ * @author    Tetsuya Yoshida <tetu@eth0.jp>
+ * @copyright 2010 Tetsuya Yoshida
+ * @license   http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version   1.1.0
+ * @link      http://openpear.org/package/HTTP_OAuthProvider
+ */
+require_once 'PHPUnit/Framework.php';
+require_once 'HTTP/OAuthProvider.php';
+require_once 'HTTP/OAuthProvider/Mock/HMAC_SHA1.php';
+
+/**
+ * HTTP_OAuthProvider_SignatureTest
+ *
+ * @category HTTP
+ * @package  OAuthProvider
+ * @author   Tetsuya Yoshida <tetu@eth0.jp>
+ * @license  http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version  1.1.0
+ * @link     http://openpear.org/package/HTTP_OAuthProvider
+ */
+class HTTP_OAuthProvider_SignatureTest extends PHPUnit_Framework_TestCase
+{
+    /** 
+     * setUp
+     * 
+     * Set up a mock object.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        $this->provider = new HTTP_OAuthProvider_Mock_HMAC_SHA1();
+    }
+
+    /** 
+     * testUrlencode
+     * 
+     * Check a url encoding.
+     *
+     * @return void
+     *
+     * @test
+     */
+    public function testUrlencode()
+    {
+        $valid = '_-%2B%2A%2F.%26~%40%5B%5D%3C%3E%28%29%21%3F%20';
+        $input = $this->provider->getRequest()->getParameter('testparam');
+        $output = HTTP_OAuthProvider_Signature::urlencode_rfc3986($input);
+        $this->assertEquals($valid, $output);
+    }
+
+    /** 
+     * testHttpBuildQuery
+     * 
+     * Check a http build query.
+     *
+     * @return void
+     *
+     * @test
+     */
+    public function testHttpBuildQuery()
+    {
+        $valid = 'oauth_consumer_key=testuser&oauth_nonce=fcaa5f84f9a15c79248ea66159d1fa72&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1284303122&oauth_version=1.0&testparam=_-%2B%2A%2F.%26~%40%5B%5D%3C%3E%28%29%21%3F%20';
+        $params = $this->provider->getRequest()->getParameter();
+        $output = HTTP_OAuthProvider_Signature::http_build_query_rfc3986($params);
+        $this->assertEquals($valid, $output);
+    }
+}
