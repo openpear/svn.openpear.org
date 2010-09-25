eafecc57f8461885821415ad9d0cb79a85a7a417
Yoshida Tetsuya <yoshida.eth0@gmail.com>
2010-09-25 18:37:48 +0000

    mod example

--- HTTP_OAuthProvider/trunk/example/consumer_http_oauthconsumer.php
+++ HTTP_OAuthProvider/trunk/example/consumer_http_oauthconsumer.php
@@ -3,7 +3,7 @@ require_once('HTTP/OAuthConsumer.php');
 
 session_start();
 
-// provider url
+// Provider info
 $provider_base = sprintf('http://%s%s', $_SERVER['HTTP_HOST'], dirname($_SERVER['SCRIPT_NAME']));
 $provider_base = rtrim($provider_base, '/').'/';
 $request_url = $provider_base.'3legged_request.php';
@@ -11,69 +11,70 @@ $authorize_url = $provider_base.'3legged_authorize.php';
 $access_url = $provider_base.'3legged_access.php';
 $resource_url = $provider_base.'3legged_resource.php';
 
-// consumer url
+// Consumer info
+$consumer_key = 'testconsumer';
+$consumer_secret = 'testpass';
 $callback_url = sprintf('http://%s%s', $_SERVER['HTTP_HOST'], $_SERVER['SCRIPT_NAME']);
 
-/* セッションのクリア */
+// Session clear
 if (@$_REQUEST['test'] === 'clear') {
 	session_destroy();
+	$_SESSION = array();
 	session_start();
 }
 
 $content = '';
 
 try {
+	// Initialize HTTP_OAuthConsumer
 	$oauth = HTTP_OAuthConsumer::factory();
-	$oauth->setConsumer('testconsumer', 'testpass');
+	$oauth->setConsumer($consumer_key, $consumer_secret);
 
 	if (!isset($_SESSION['type'])) {
 		$_SESSION['type'] = null;
 	}
 
-	// 2 認可をもらって帰ってきた
+	// 2 Authorize
 	if ($_SESSION['type']=='authorize' && isset($_GET['oauth_token'], $_GET['oauth_verifier'])) {
-
-		// リクエストトークンをアクセストークンに交換
+		// Exchange the Request Token for an Access Token
 		$oauth->setURL($access_url);
 		$oauth->setToken($_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
 		$access = $oauth->getAccessToken($_GET['oauth_verifier']);
 
-		// アクセストークンを保存
+		// Save an Access Token
 		$_SESSION['type'] = 'access';
 		$_SESSION['oauth_token'] = $access['oauth_token'];
 		$_SESSION['oauth_token_secret'] = $access['oauth_token_secret'];
 	}
 
-	// 3 保護されたリソースへアクセス
+	// 3 Access
 	if ($_SESSION['type']=='access') {
-		// 保護されたリソースへリクエストを送る
+		// Accessing Protected Resources
 		$oauth->setURL($resource_url);
 		$oauth->setToken($_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
-		$res = $oauth->send();
+		$result = $oauth->send();
 
-		// データを取得
-		$content = $res->getBody();
+		$content = $result->getBody();
 
-	// 1 認可をもらいに飛ぶ
+	// 1 Request
 	} else {
-		// プロバイダからリクエストトークンの取得
+		// Get a Request Token
 		$oauth->setURL($request_url);
-		$req = $oauth->getRequestToken($callback_url);
+		$request = $oauth->getRequestToken($callback_url);
 
-		// リクエストトークンをセッションに保存
+		// Save a Request Token
 		$_SESSION['type'] = 'authorize';
-		$_SESSION['oauth_token'] = $req['oauth_token'];
-		$_SESSION['oauth_token_secret'] = $req['oauth_token_secret'];
+		$_SESSION['oauth_token'] = $request['oauth_token'];
+		$_SESSION['oauth_token_secret'] = $request['oauth_token_secret'];
 
-		// authorize urlを取得
+		// Get an Authorize URL
 		$authorize_url = $oauth->getAuthorizeURL($authorize_url);
 
-		// authorize urlのリンク作成
 		$content = "Click on the link to go to provider to authorize your account.<br />\n";
 		$content .= sprintf('<a href="%s">%s</a>', $authorize_url, $authorize_url);
 	}
 
-} catch(Exception $e) {
+} catch (Exception $e) {
 	$content .= $e->getMessage();
 }
 ?>
@@ -83,7 +84,7 @@ try {
 </head>
 <body>
 <h2>Welcome to a OAuth PHP example.</h2>
-<p><a href='<?php echo $_SERVER['PHP_SELF']; ?>?test=clear'>clear sessions</a></p>
+<p><a href='?test=clear'>clear sessions</a></p>
 
 <p><pre><?php print_r($content); ?><pre></p>
 
