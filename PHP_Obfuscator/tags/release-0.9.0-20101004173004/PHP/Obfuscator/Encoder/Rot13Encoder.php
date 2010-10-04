10ae9189967a93625b3f9e60c6248a7202f09a3a
openpear <openpear@224e63c0-5457-0410-989b-907d9cf58824>
2010-10-04 08:30:04 +0000

    [Package Release] PHP_Obfuscator

---
+++ PHP_Obfuscator/tags/release-0.9.0-20101004173004/PHP/Obfuscator/Encoder/Rot13Encoder.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * PHP_Obfuscator_Encoder_Rot13Encoder class
+ *
+ * PHP version 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy
+ * the PHP License and are unable to obtain it through the web,
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category  PHP
+ * @package   PHP_Obfuscator
+ * @author    Hideyuki Shimooka <shimooka@doyouphp.jp>
+ * @copyright 2010 Hideyuki Shimooka
+ * @license   http://www.php.net/license/3_01.txt The PHP License, version 3.01
+ * @version   SVN: $Id$
+ * @link      http://openpear.org/package/PHP_Obfuscator
+ */
+
+require_once 'PHP/Obfuscator/Encoder/AbstractEncoder.php';
+
+/**
+ * PHP_Obfuscator_Encoder_Rot13Encoder class
+ *
+ * @category  PHP
+ * @package   PHP_Obfuscator
+ * @author    Hideyuki Shimooka <shimooka@doyouphp.jp>
+ * @copyright 2010 Hideyuki Shimooka
+ * @license   http://www.php.net/license/3_01.txt The PHP License, version 3.01
+ * @version   Release: @package_version@
+ * @link      http://openpear.org/package/PHP_Obfuscator
+ */
+class PHP_Obfuscator_Encoder_Rot13Encoder extends PHP_Obfuscator_Encoder_AbstractEncoder
+{
+
+    /**
+     * Return encoded previous code
+     *
+     * @return string Return encoded code
+     * @access public
+     */
+    public function encode() {
+        return str_rot13($this->str);
+    }
+
+    /**
+     * Return the code for decoding
+     *
+     * @return string The code for decoding in 'sprintf/printf' format
+     * @access public
+     */
+    public function decode() {
+        return 'str_rot13(%s)';
+    }
+}
