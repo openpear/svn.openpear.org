1a2c7bd30206d6eba4642c0a7a6b8d000c757602
FreeBSE <freebse@panasocli.cc>
2010-10-06 10:27:43 +0000

    携帯圧縮メソッド使用時におけるインスタンス生成の引数をブール代数に変更 +実験 -GDなしでも画像を表示できるメソッドの追加 -ファイル名とクラス名が違ってても大丈夫なクラスファクトリーの追加

--- PHP_PowerToys/trunk/PowerToys.php
+++ PHP_PowerToys/trunk/PowerToys.php
@@ -1,213 +1,221 @@
 <?php
 /**
- *  PHP_PowerToys 0.2.9
+ *  PHP_PowerToys Current
  *
  *  @author	    FreeBSE <freebse@live.jp> <http://panasocli.cc/wordpress>
  *  @package	PHP_PowerToys
- *  @version	PHP_PowerToys v 0.2.9
+ *  @version	PHP_PowerToys Current
  * 
  */
 class PHP_PowerToys {
 	
-	function PHP_PowerToys($option=null){
-		if($option==='compress') {
-		    require_once 'Net/UserAgent/Mobile.php';
-		    $mobile =& Net_UserAgent_Mobile::factory();
-		    if (!$mobile->isDocomo() || !$mobile->isSoftbank() || !$mobile->isEZWeb()) return false;
-		    ob_start();
-		}
-	}
-	
-	/**
-	 * 通常のクラスファクトリー
-	 *
-	 * @param string $class
-	 * @param mixed $arg 配列か文字列か数値かは、インスタンス生成時によって適宜対応
-	 * @return object
-	 */
-	function getInstance($class, $arg=null){
-		require_once($class);
-		$class = str_replace('.php', '', $class);
-		return new $class($arg);
-	}
-	
-	/**
-	 * PEARライブラリのクラスファクトリー(インストール風)
-	 *
-	 * @param string $pear
-	 * @param mixed $arg 配列か文字列か数値かは、PEARインスタンス生成時によって適宜対応
-	 * @return object
-	 */
-	function getPearI($pear, $arg=null){
-		require_once(strtr('_', '/', $pear) . '.php');
-		return new $pear($arg);
-	}
-	
-	/**
-	 * PEARライブラリのクラスファクトリー
-	 *
-	 * @param string $pear
-	 * @param mixed $arg 配列か文字列か数値かは、PEARインスタンス生成時によって適宜対応
-	 * @return object
-	 */
-	function getPear($pear, $arg=null){
-		require_once($pear);
-		$pear = strtr('/', '_', $pear);
-		$pear = str_replace('.php', '', $pear);
-		return new $pear($arg);
-	}
-	
-	/**
-	 * 配列の順序を保ったまま、一部のキー名を変更する
-	 *
-	 * @param array $array
-	 * @param string $from
-	 * @param string $to
-	 * @return Array
-	 */
-	function arrayKeyReplace($array, $from, $to){
-		foreach($array as $key => $val){
-			$key = $key == $from ? $to : $key;
-			$tmp[$key] = $val;
-		}
-		unset($array);
-		return $tmp;
-	}
-	
-	/**
-	 *ファイルの存在をチェックする Include_pathも含める
-	 * 
-	 * @param $file_path ファイルのパス
-	 * @access public
-	 * @return true 成功 false 失敗
-	 */	
-	function is_file_ex($file_path){
-		if(is_file($file_path)) return true;
-
-		$include = explode(';', ini_get('include_path'));
-		array_shift($include);
-		foreach($include as $inc){
-			if(is_file($inc . $file_path)){
-				return true;
-			}
-		}
-
-		$include = split(':|;', ini_get('include_path'));
-		foreach($include as $inc){
-			if($inc === '.' && is_file($inc . '/' . $file_path)) return true;
-			if(is_file($inc . '/' . $file_path)){
-				return true;
-			}
-		}
-
-		return false;
-	}
-	
-	/**
-	 * var_dumpの結果を保存する
-	 *
-	 * @param Array $var
-	 * @param string $filename
-	 */
-	function save_var_dump($var, $filename){
+    /**
+     * コンストラクタ
+     * @param Boolean $compress
+     * 
+     */
+    function PHP_PowerToys($compress=false){
+	    if($compress===true) {
+		require_once 'Net/UserAgent/Mobile.php';
+		$mobile =& Net_UserAgent_Mobile::factory();
+		if (!$mobile->isDocomo() || !$mobile->isSoftbank() || !$mobile->isEZWeb()) return false;
 		ob_start();
+	    }
+    }
+	
+    /**
+     * 通常のクラスファクトリー
+     *
+     * @param string $class
+     * @param mixed $arg 配列か文字列か数値かは、インスタンス生成時によって適宜対応
+     * @return object
+     */
+    function getInstance($class, $arg=null){
+	    require_once($class);
+	    $class = str_replace('.php', '', $class);
+	    return new $class($arg);
+    }
+
+    /**
+     * PEARライブラリのクラスファクトリー(インストール風)
+     *
+     * @param string $pear
+     * @param mixed $arg 配列か文字列か数値かは、PEARインスタンス生成時によって適宜対応
+     * @return object
+     */
+    function getPearI($pear, $arg=null){
+	    require_once(strtr('_', '/', $pear) . '.php');
+	    return new $pear($arg);
+    }
+
+    /**
+     * PEARライブラリのクラスファクトリー
+     *
+     * @param string $pear
+     * @param mixed $arg 配列か文字列か数値かは、PEARインスタンス生成時によって適宜対応
+     * @return object
+     */
+    function getPear($pear, $arg=null){
+	    require_once($pear);
+	    $pear = str_replace('.php', '', strtr('/', '_', $pear));
+	    return new $pear($arg);
+    }
+
+    /**
+     * 配列の順序を保ったまま、一部のキー名を変更する
+     *
+     * @param array $array
+     * @param string $from
+     * @param string $to
+     * @return Array
+     */
+    function arrayKeyReplace($array, $from, $to){
+	    foreach($array as $key => $val){
+		    $key = $key == $from ? $to : $key;
+		    $tmp[$key] = $val;
+	    }
+	    unset($array);
+	    return $tmp;
+    }
+
+    /**
+     *ファイルの存在をチェックする Include_pathも含める
+     * 
+     * @param $file_path ファイルのパス
+     * @access public
+     * @return true 成功 false 失敗
+     */	
+    function is_file_ex($file_path){
+	    if(is_file($file_path)) return true;
+
+	    $include = explode(';', ini_get('include_path'));
+	    array_shift($include);
+	    foreach($include as $inc){
+		    if(is_file($inc . $file_path)){
+			    return true;
+		    }
+	    }
+
+	    $include = split(':|;', ini_get('include_path'));
+	    foreach($include as $inc){
+		    if($inc === '.' && is_file($inc . '/' . $file_path)) return true;
+		    if(is_file($inc . '/' . $file_path)){
+			    return true;
+		    }
+	    }
+
+	    return false;
+    }
+
+    /**
+     * var_dumpの結果を保存する
+     *
+     * @param Array $var
+     * @param string $filename
+     */
+    function save_var_dump($var, $filename, $tag=null, $end_tag=null){
+	    ob_start();
+	    if(is_null($tag)){
 		var_dump($var);
-		$v = ob_get_contents();
-		ob_clean();
-		$f = fopen($filename, "w");
-		fputs($f, $var);
-		fclose($f);
-	}
-	
-	/**
-	 * 指定されたファイル名もしくは文字列にBOMがあるかチェックする
-	 *
-	 * @param string $str
-	 * @return false BOMなし or UTF-8ではない true BOMつきUTF-8
-	 */
-	function checkBom($str){
-		if(PHP_Powertoys::is_file_ex($str)){
-			$str = file_get_contents($str);
-		}
-		if(mb_detect_encoding($str) !== 'UTF-8') return false;
-		if (ord($str{0}) == 0xef && ord($str{1}) == 0xbb && ord($str{2}) == 0xbf) {
-        	return true;
-    	}
-    	return false;
-	}
-	
-	/**
-	 * 指定されたファイル名もしくは文字列にBOMがあれば除去する
-	 *
-	 * @param string $str
-	 * @return string
-	 */
-	function removeBom($str){
+	    }else{
+		var_dump($tag, $var, $end_tag);
+	    }
+	    $v = ob_get_contents();
+	    ob_clean();
+	    $f = fopen($filename, "w");
+	    fputs($f, $var);
+	    fclose($f);
+    }
+
+    /**
+     * 指定されたファイル名もしくは文字列にBOMがあるかチェックする
+     *
+     * @param string $str
+     * @return false BOMなし or UTF-8ではない true BOMつきUTF-8
+     */
+    function checkBom($str){
 	    if(PHP_Powertoys::is_file_ex($str)){
-		$str = file_get_contents($str);
+		    $str = file_get_contents($str);
 	    }
 	    if(mb_detect_encoding($str) !== 'UTF-8') return false;
 	    if (ord($str{0}) == 0xef && ord($str{1}) == 0xbb && ord($str{2}) == 0xbf) {
-		    $str = substr($str, 3);
-	    }
-	    return $str;
-	}
-	
-	/**
-	 * 指定されたファイル名もしくは文字列にBOMがなければ付加する(うわぁぁぁ死ねばいいのに・・・)
-	 *
-	 * @param string $str
-	 * @return string
-	 */
-	function addBom($str){
-	    if(PHP_Powertoys::is_file_ex($str)){
-		$str = file_get_contents($str);
-	    }
-	    if(mb_detect_encoding($str) != 'UTF-8') return false;
-	    if (ord($str{0}) != 0xef && ord($str{1}) != 0xbb && ord($str{2}) != 0xbf) {
-		$str = chr(0xef) . chr(0xbb) . chr(0xbf) . $str;
-	    }
-	    return $str;
-	}
-	
-	/**
-	 * 拡張(PHPエクステンション)があるかチェックする
-	 *
-	 * @param string $extension
-	 * @return 成功:TRUE 失敗:FALSE
-	 */
-	function extensionExist($extension){
-	    $extension_soname = $extension . '.' . PHP_SHLIB_SUFFIX;
-	    $extension_fullname = PHP_EXTENSION_DIR . '/' . $extension_soname;
-	    if(!extension_loaded($extension) && !is_file($extension_fullname)) {
-		return false;
-	    }
 	    return true;
+    }
+    return false;
+    }
+
+    /**
+     * 指定されたファイル名もしくは文字列にBOMがあれば除去する
+     *
+     * @param string $str
+     * @return string
+     */
+    function removeBom($str){
+	if(PHP_Powertoys::is_file_ex($str)){
+	    $str = file_get_contents($str);
 	}
-	
-	/**
-	 * 画像を自動認識してGDのリソース情報を返す
-	 *
-	 * @param string $img 画像フルパスにて
-	 * @return 成功:GD Resource 失敗:False
-	 */
-	function iopen($img){
-		//GDがインストールされているかチェックする
-		 if(!PHP_PowerToys::extensionExist('gd')) return false;
-		$content = file_get_contents($img);
-		if ( preg_match( '/^\x89PNG\x0d\x0a\x1a\x0a/', $content) )  {
-			$gd = imagecreatefrompng($img);
-		} elseif ( preg_match( '/^GIF8[79]a/', $content) )  {
-			$gd = imagecreatefromgif($img);
-		} elseif ( preg_match( '/^\xff\xd8/', $content) )  {
-			$gd = imagecreatefromjpeg($img);
-		} elseif ( preg_match( '/^BM|^\x42\x4d/', $content) )  {
-			$gd = PHP_PowerToys::ImageCreateFromBMP($img);
-		}else{
-			return false;
-		}
-		return $gd;
+	if(mb_detect_encoding($str) !== 'UTF-8') return false;
+	if (ord($str{0}) == 0xef && ord($str{1}) == 0xbb && ord($str{2}) == 0xbf) {
+		$str = substr($str, 3);
 	}
+	return $str;
+    }
+
+    /**
+     * 指定されたファイル名もしくは文字列にBOMがなければ付加する(うわぁぁぁ死ねばいいのに・・・)
+     *
+     * @param string $str
+     * @return string
+     */
+    function addBom($str){
+	if(PHP_Powertoys::is_file_ex($str)){
+	    $str = file_get_contents($str);
+	}
+	if(mb_detect_encoding($str) != 'UTF-8') return false;
+	if (ord($str{0}) != 0xef && ord($str{1}) != 0xbb && ord($str{2}) != 0xbf) {
+	    $str = chr(0xef) . chr(0xbb) . chr(0xbf) . $str;
+	}
+	return $str;
+    }
+
+    /**
+     * 拡張(PHPエクステンション)があるかチェックする
+     *
+     * @param string $extension
+     * @return 成功:TRUE 失敗:FALSE
+     */
+    function extensionExist($extension){
+	$extension_soname = $extension . '.' . PHP_SHLIB_SUFFIX;
+	$extension_fullname = PHP_EXTENSION_DIR . '/' . $extension_soname;
+	if(!extension_loaded($extension) && !is_file($extension_fullname)) {
+	    return false;
+	}
+	return true;
+    }
+
+    /**
+     * 画像を自動認識してGDのリソース情報を返す
+     *
+     * @param string $img 画像フルパスにて
+     * @return 成功:GD Resource 失敗:False
+     */
+    function iopen($img){
+	    //GDがインストールされているかチェックする
+	     if(!PHP_PowerToys::extensionExist('gd')) return false;
+	    $content = file_get_contents($img);
+	    if ( preg_match( '/^\x89PNG\x0d\x0a\x1a\x0a/', $content) )  {
+		    $gd = imagecreatefrompng($img);
+	    } elseif ( preg_match( '/^GIF8[79]a/', $content) )  {
+		    $gd = imagecreatefromgif($img);
+	    } elseif ( preg_match( '/^\xff\xd8/', $content) )  {
+		    $gd = imagecreatefromjpeg($img);
+	    } elseif ( preg_match( '/^BM|^\x42\x4d/', $content) )  {
+		    $gd = PHP_PowerToys::ImageCreateFromBMP($img);
+	    }else{
+		    return false;
+	    }
+	    return $gd;
+    }
 	
 	//from http://jp.php.net/manual/ja/function.imagecreate.php#53879
 	/*********************************************/
@@ -704,6 +712,41 @@ class PHP_PowerToys {
 	preg_match("/Parse error: syntax error, unexpected '}' in/", $sudo_php)){
 	    return 'パースエラー:文法エラー「{」が多く存在します';
 	}
+    }
+    
+    /**
+     * GDなしでも画像読み込み&表示(Experiment)
+     * 
+     * @param <type> $image 
+     */
+    function gdNotImageView($image){
+	$f = file_get_contents($image);
+	if ( preg_match( '/^\x89PNG\x0d\x0a\x1a\x0a/', $f) )  {
+		header("Content-type: image/png");
+	} elseif ( preg_match( '/^GIF8[79]a/', $f) )  {
+		header("Content-type: image/gif");
+	} elseif ( preg_match( '/^\xff\xd8/', $f) )  {
+		header("Content-type: image/jpeg");
+	} elseif ( preg_match( '/^BM|^\x42\x4d/', $f) )  {
+		header("Content-type: image/bmp");
+	}else{
+		return false;
+	}
+	echo $f;
+    }
+    
+    /**
+     * 自動認識クラスファクトリーでインスタンスを返す(Experiment)
+     * 
+     * @param <type> $php
+     * @return class 
+     */
+    function &classFactory($php){
+	require_once($php);
+//	$php = preg_split("/\\|\/|\./", $php);
+//	$php = $php[count($php) - 2];
+	$class = get_declared_classes();
+	return new $class[count($class) -1]();
     }
 }
 
