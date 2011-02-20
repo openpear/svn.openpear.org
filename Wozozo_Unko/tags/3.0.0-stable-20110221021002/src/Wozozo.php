b1599f225228eac6a0ac9cf4e847899d6d418b39
openpear <openpear@224e63c0-5457-0410-989b-907d9cf58824>
2011-02-20 17:10:02 +0000

    package released (3.0.0-stable) (@riaf)

---
+++ Wozozo_Unko/tags/3.0.0-stable-20110221021002/src/Wozozo.php
@@ -0,0 +1,18 @@
+<?php
+/** 
+ * 
+ * 
+ */
+
+class Wozozo
+{
+    public function __construct()
+    {
+        print "wozozo is hello";
+    }
+
+    public function __destruct()
+    {
+    	echo 'ヽ( ・∀・)ﾉ ｳﾝｺｰー！'
+    }
+}
