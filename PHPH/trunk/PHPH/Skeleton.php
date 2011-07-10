b39f2e4a171eafeb393025ce6e263d2b13ac9d80
eth0jp <eth0jp@224e63c0-5457-0410-989b-907d9cf58824>
2011-07-10 01:48:41 +0000

    mv skeleton

--- PHPH/trunk/PHPH/Skeleton.php
+++ PHPH/trunk/PHPH/Skeleton.php
@@ -5,37 +5,37 @@ class PHPH_Skeleton
 {
 	public static function loadConfigM4()
 	{
-		$path = dirname(__FILE__)."/Skeleton/config.m4";
+		$path = dirname(__FILE__)."/Skeleton/config.m4.php";
 		return file_get_contents($path);
 	}
 
 	public static function loadConfigW32()
 	{
-		$path = dirname(__FILE__)."/Skeleton/config.w32";
+		$path = dirname(__FILE__)."/Skeleton/config.w32.php";
 		return file_get_contents($path);
 	}
 
 	public static function loadH()
 	{
-		$path = dirname(__FILE__)."/Skeleton/php_skeleton.h";
+		$path = dirname(__FILE__)."/Skeleton/php_skeleton.h.php";
 		return file_get_contents($path);
 	}
 
 	public static function loadC()
 	{
-		$path = dirname(__FILE__)."/Skeleton/php_skeleton.c";
+		$path = dirname(__FILE__)."/Skeleton/php_skeleton.c.php";
 		return file_get_contents($path);
 	}
 
 	public static function loadMain()
 	{
-		$path = dirname(__FILE__)."/Skeleton/skeleton.c";
+		$path = dirname(__FILE__)."/Skeleton/skeleton.c.php";
 		return file_get_contents($path);
 	}
 
 	public static function loadDSP()
 	{
-		$path = dirname(__FILE__)."/Skeleton/skeleton.dsp";
+		$path = dirname(__FILE__)."/Skeleton/skeleton.dsp.php";
 		return file_get_contents($path);
 	}
 
