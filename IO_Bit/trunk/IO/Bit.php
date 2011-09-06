266bcdab361618a90f32717064ecaa9080713588
yoya <yoya@224e63c0-5457-0410-989b-907d9cf58824>
2011-09-06 06:55:17 +0000

    setUI16LE を追加

--- IO_Bit/trunk/IO/Bit.php
+++ IO_Bit/trunk/IO/Bit.php
@@ -403,6 +403,12 @@ class IO_Bit {
     /*
      * set method
      */
+    function setUI16LE($value, $byte_offset) {
+        $data = pack('v', $value);
+        $this->_data{$byte_offset + 0} = $data{0};
+        $this->_data{$byte_offset + 1} = $data{1};
+        return true;
+    }
     function setUI32LE($value, $byte_offset) {
         $data = pack('V', $value);
         $this->_data{$byte_offset + 0} = $data{0};
