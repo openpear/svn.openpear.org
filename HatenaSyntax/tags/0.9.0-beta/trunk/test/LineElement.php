e25b4184a3ae9c86784c11d40901c3ceca044298
anatoo <anatoo.jp@gmail.com>
2011-09-29 18:42:09 +0000

    oops

--- HatenaSyntax/tags/0.9.0-beta/trunk/test/LineElement.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once dirname(__FILE__) . '/t/t.php';
-
-$lime = new lime_test;
-$elt = HatenaSyntax_Locator::it()->lineElement;
-
-//--
-$context = context('a');
-$lime->is($elt->parse($context), 'a');
