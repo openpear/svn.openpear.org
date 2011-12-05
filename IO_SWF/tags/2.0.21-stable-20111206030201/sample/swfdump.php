41d4f2ae34c62d1416f7be56da2a0c079b1e1c2c
openpear <openpear@224e63c0-5457-0410-989b-907d9cf58824>
2011-12-05 18:02:02 +0000

    package released (2.0.21-stable) (@yoya)

---
+++ IO_SWF/tags/2.0.21-stable-20111206030201/sample/swfdump.php
@@ -0,0 +1,27 @@
+<?php
+
+require 'IO/SWF.php';
+// require dirname(__FILE__).'/../IO/SWF.php';
+
+$options = getopt("f:h");
+
+if (is_readable($options['f']) === false) {
+    echo "Usage: php swfdump.php -f <swf_file> [-h]\n";
+    echo "ex) php swfdump.php -f test.swf -h \n";
+    exit(1);
+}
+
+$swfdata = file_get_contents($options['f']);
+
+$swf = new IO_SWF();
+
+$swf->parse($swfdata);
+
+$opts = array();
+if (isset($options['h'])) {
+    $opts['hexdump'] = true;
+}
+
+$swf->dump($opts);
+
+exit(0);
