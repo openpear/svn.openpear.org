8db57099c049155f0532b8e80013ba6e515ec674
よや <yoya@awm.jp>
2014-01-20 06:39:35 +0000

    filename - => stdin

--- IO_Zlib/trunk/sample/zlibdump.php
+++ IO_Zlib/trunk/sample/zlibdump.php
@@ -2,7 +2,13 @@
 
 require_once('IO/Zlib.php');
 
-$zlibdata = file_get_contents($argv[1]);
+$filename = $argv[1];
+
+if ($filename === '-')  {
+   $filename = 'php://stdin';
+}
+
+$zlibdata = file_get_contents($filename);
 
 $zip = new IO_Zlib();
 $zip->parse($zlibdata);
