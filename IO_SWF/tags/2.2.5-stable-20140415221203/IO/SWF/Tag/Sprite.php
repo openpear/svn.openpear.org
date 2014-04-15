29a56d215532e679d0f4a1e6785d5743200d218a
openpear <openpear@224e63c0-5457-0410-989b-907d9cf58824>
2014-04-15 13:12:03 +0000

    package released (2.2.5-stable) (@yoya)

---
+++ IO_SWF/tags/2.2.5-stable-20140415221203/IO/SWF/Tag/Sprite.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once 'IO/Bit.php';
+require_once dirname(__FILE__).'/Base.php';
+require_once dirname(__FILE__).'/../Tag.php';
+
+class IO_SWF_Tag_Sprite extends IO_SWF_Tag_Base {
+    var $_spriteId = null;
+    var $_frameCount = null;
+    var $_controlTags = array();
+    function parseContent($tagCode, $content, $opts = array()) {
+        $reader = new IO_Bit();
+    	$reader->input($content);
+        
+        $this->_spriteId = $reader->getUI16LE();
+        $this->_frameCount = $reader->getUI16LE();
+        /* SWF Tags */
+        while (true) {
+            $tag = new IO_SWF_Tag($this->swfInfo);
+            $tag->parse($reader);
+            $this->_controlTags[] = $tag;
+            if ($tag->code == 0) { // END Tag
+                break;
+            }
+        }
+        return true;
+    }
+    
+    function dumpContent($tagCode, $opts = array()) {
+        echo "\tSprite: SpriteID={$this->_spriteId} FrameCount={$this->_frameCount}\n";
+        foreach ($this->_controlTags as $tag) {
+            echo "  ";
+            try {
+                $tag->dump($opts);
+            } catch (IO_Bit_Exception $e) {
+                echo "(tag parse failed)\n";
+            }
+        }
+    }
+    
+    function buildContent($tagCode, $opts = array()) {
+        $writer = new IO_Bit();
+        $writer->putUI16LE($this->_spriteId);
+        $writer->putUI16LE($this->_frameCount);
+        foreach ($this->_controlTags as $tag) {
+            $tagData = $tag->build();
+            if ($tagData != false) {
+                $writer->putData($tagData);
+            }
+        }
+    	return $writer->output();
+    }
+}
