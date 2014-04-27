df3ecd2927d6ccf450f189416f7aa393b60dc580
openpear <openpear@224e63c0-5457-0410-989b-907d9cf58824>
2014-04-27 16:43:02 +0000

    package released (2.2.6-stable) (@yoya)

---
+++ IO_SWF/tags/2.2.6-stable-20140428014302/IO/SWF/Type/Float.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * 2011/6/27- (c) yoya@awm.jp
+ */
+
+require_once 'IO/Bit.php';
+require_once dirname(__FILE__).'/../Type.php';
+
+class IO_SWF_Type_Float implements IO_SWF_Type {
+    static function parse(&$reader, $opts = array()) {
+        $data = $reader->getData(4);
+	$unpacked_data = unpack('f', $data);
+    	return (float)$unpacked_data[1];
+    }
+    static function build(&$writer, $value, $opts = array()) {
+        $data = pack('f', (float)$value);
+    	$writer->putData($data, 4);
+    }
+    static function string($value, $opts = array()) {
+    	return sprintf("(Float)%f", $value);
+    }
+}
