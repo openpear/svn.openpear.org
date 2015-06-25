a6f13c3c29b9fa87ca7861da8d835e66899375e5
openpear <openpear@224e63c0-5457-0410-989b-907d9cf58824>
2015-06-25 10:53:03 +0000

    package released (2.2.9-stable) (@yoya)

---
+++ IO_SWF/tags/2.2.9-stable-20150625195303/IO/SWF/Type/Double.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * 2011/6/27- (c) yoya@awm.jp
+ */
+
+require_once 'IO/Bit.php';
+require_once dirname(__FILE__).'/../Type.php';
+
+class IO_SWF_Type_Double implements IO_SWF_Type {
+    static function parse(&$reader, $opts = array()) {
+        $data = $reader->getData(8);
+	$unpacked_data = unpack('d', $data);
+    	return (double)$unpacked_data[1];
+    }
+    static function build(&$writer, $value, $opts = array()) {
+        $data = pack('d', (double)$value);
+    	$writer->putData($data, 8);
+    }
+    static function string($value, $opts = array()) {
+    	return sprintf("(Double)%d", $value);
+    }
+}
